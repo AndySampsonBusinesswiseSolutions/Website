31b5d0b3292d0b240d7fc422344924a987aafe03
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2019-11-21 10:29:13 +0000

    32-portfolio-management-view-meter-consumption redesign
    Chart headers now resize dynamically

--- UI/portfoliomanagement/meterconsumption/gaschart.php
+++ UI/portfoliomanagement/meterconsumption/gaschart.php
@@ -4,34 +4,38 @@
 	<div>
 		<div class="group-div">
 			<div class="group-by-div">
-				<span class="fas fa-chart-line" style="padding-right: 5px"></span>
-				<span class="chart-header">Need Graph Title Here</span>
-				<span style="padding-right: 5px;" class="simple-divider"></span>
-				<span>Show By:</span>
-				<span class="show-pointer">
-					<span class="arrow-header" title="Show By" style="padding-left: 10px;" id="gasChartHeaderShowBy">Energy</span>
-					<span class="fa fa-angle-double-down" style="padding-left: 10px;" id="gasChartHeaderShowByArrow"></span>				
-				</span>
-				<span style="padding-right: 5px;" class="simple-divider"></span>
-				<span>Period:</span>
-				<span class="show-pointer">
-					<span class="arrow-header" title="Period" style="padding-left: 10px;" id="gasChartHeaderPeriod">Monthly</span>
-					<span class="fa fa-angle-double-down" style="padding-left: 10px;" id="gasChartHeaderPeriodArrow"></span>				
-				</span>
-				<span class="simple-divider"></span>
-				<span title="Previous Period" class="fas fa-caret-left show-pointer"></span>
-				<span title="Current Period" class="show-pointer">Nov 2019</span>
-				<span title="Next Period" class="fas fa-caret-right show-pointer"></span>
-				<span></span>
-				<span title="Calendar" class="fas fa-calendar-alt show-pointer"></span>
-				<span class="simple-divider"></span>
-				<span title="Chart Type" class="fas fa-chart-bar show-pointer"></span>
-				<span class="simple-divider"></span>
-				<span title="Layers" class="fas fa-layer-group show-pointer"></span>
-				<span class="simple-divider"></span>
-				<span title="Download" class="fas fa-download show-pointer"></span>
-				<span class="simple-divider"></span>
-				<span title="Refresh" class="fas fa-sync show-pointer"></span>
+				<div style="width: 30%; display: inline-block;">
+					<span class="fas fa-chart-line" style="padding-right: 5px"></span>
+					<span class="chart-header">Gas Time View</span>
+				</div>
+				<div style="display: inline-block; float: right;">
+					<span style="padding-right: 5px;" class="simple-divider"></span>
+					<span>Show By:</span>
+					<span class="show-pointer">
+						<span class="arrow-header" title="Show By" style="padding-left: 10px;" id="gasChartHeaderShowBy">Energy</span>
+						<span class="fa fa-angle-double-down" style="padding-left: 10px;" id="gasChartHeaderShowByArrow"></span>				
+					</span>
+					<span style="padding-right: 5px;" class="simple-divider"></span>
+					<span>Period:</span>
+					<span class="show-pointer">
+						<span class="arrow-header" title="Period" style="padding-left: 10px;" id="gasChartHeaderPeriod">Monthly</span>
+						<span class="fa fa-angle-double-down" style="padding-left: 10px;" id="gasChartHeaderPeriodArrow"></span>				
+					</span>
+					<span class="simple-divider"></span>
+					<span title="Previous Period" class="fas fa-caret-left show-pointer"></span>
+					<span title="Current Period" class="show-pointer">Nov 2019</span>
+					<span title="Next Period" class="fas fa-caret-right show-pointer"></span>
+					<span></span>
+					<span title="Calendar" class="fas fa-calendar-alt show-pointer"></span>
+					<span class="simple-divider"></span>
+					<span title="Chart Type" class="fas fa-chart-bar show-pointer"></span>
+					<span class="simple-divider"></span>
+					<span title="Layers" class="fas fa-layer-group show-pointer"></span>
+					<span class="simple-divider"></span>
+					<span title="Download" class="fas fa-download show-pointer"></span>
+					<span class="simple-divider"></span>
+					<span title="Refresh" class="fas fa-sync show-pointer"></span>
+				</div>
 			</div>
 		</div>
 	</div>
--- UI/portfoliomanagement/viewmeterconsumption.php
+++ UI/portfoliomanagement/viewmeterconsumption.php
@@ -32,55 +32,6 @@
 		</div>
 	</div>
 	<br>
-	
-	<!-- <div class="row">
-		<div class="column">
-			<span>Click on the text to show/hide meters and submeters.</span>
-			<div></div>
-			<span>View as: </span>
-			<select id="chartType" onchange="updateChartType()" class="dropdown">
-				<option value = "line">Line Graph</option>
-				<option value = "bar">Bar Chart</option>
-				<option value = "stackedBar">Stacked Bar Chart</option>
-				<option value = "horizontalBar">Horizontal Bar Chart</option>
-				<option value = "horizontalStackedBar">Horizontal Stacked Bar Chart</option>
-			</select>
-			<div>
-				<input class="child-check-input" type="checkbox" name="Total Of All Sites" id="0"><label>Total Of All Sites</label>
-			</div>
-			<div class="parent-check">
-				<input class="child-check-input" type="checkbox" name="Site 1" id="1"><label>Site 1</label>
-				<div class="child-check">
-					<input class="child-check-input" type="checkbox" name="MPAN 1" id="2"><label>MPAN 1</label>
-					<div class="child-check">
-						<input class="child-check-input" type="checkbox" name="SubMeter 1" id="3"><label>SubMeter 1</label>
-					</div>
-					<div class="child-check">
-						<input class="child-check-input" type="checkbox" name="SubMeter 2" id="4"><label>SubMeter 2</label>
-					</div>
-				</div>
-			</div>
-			<div class="parent-check">
-				<input class="child-check-input" type="checkbox" name="Site 2" id="5"><label>Site 2</label>
-				<div class="child-check">
-					<input class="child-check-input" type="checkbox" name="MPAN 2" id="6"><label>MPAN 2</label>
-				</div>
-			</div>
-		</div>
-		<div class="finalcolumn" id="rightFrame">
-			<div>
-				<div class="chartjs-size-monitor">
-					<div class="chartjs-size-monitor-expand">
-						<div class=""></div>
-					</div>
-					<div class="chartjs-size-monitor-shrink">
-						<div class=""></div>
-					</div>
-				</div>
-				<canvas id="canvas" class="chartjs-render-monitor"></canvas>
-			</div>
-		</div>
-	</div> -->
 </body>
 
 <script src="/javascript/utils.js"></script>
@@ -109,35 +60,21 @@
 	}
 
 	window.onload = function(){
-		resizeCharts();
+		resizeCharts(365);
 	}
 
 	window.onresize = function(){
-		resizeCharts();
+		resizeCharts(365);
 	}
 
-	function resizeCharts(){
+	function resizeCharts(windowWidthReduction){
 		var finalColumns = document.getElementsByClassName("final-column");
-		var chartWidth = window.innerWidth - 365;
-		var chartHeaderWidth = chartWidth - 705;
-		
+		var chartWidth = window.innerWidth - windowWidthReduction;
+
 		for(var i=0; i<finalColumns.length; i++){
 			finalColumns[i].setAttribute("style", "width: "+chartWidth+"px;");
 		}
-
-		var chartHeaders = document.getElementsByClassName("chart-header");
-		for(var i=0; i<finalColumns.length; i++){
-			chartHeaders[i].setAttribute("style", "padding-right: "+chartHeaderWidth+"px; display: inline;");
-		}
 	}
 </script> 
-
-<!-- 
-<script>
-	var datasets = [,];
-	getDummyDataSets(datasets);
-	initialiseTree(datasets);
-	initialiseGraph(window, document, datasets);
-</script> -->
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
