44233dbc9b2b53c39a872e090cb491176261d527
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2019-12-05 10:50:49 +0000

    3-portfolio-management-site-management display-attributes
    Javascript to build display
    Updated dummy data to include attributes
    CSS to display nicely

--- UI/javascript/utils.js
+++ UI/javascript/utils.js
@@ -168,3 +168,112 @@ function resizeFinalColumns(windowWidthReduction){
 	  finalColumns[i].setAttribute('style', 'width: '+elementWidth+'px;');
 	}
   }
+
+function buildDataTable(){
+	var treeDiv = document.getElementById('displayAttributes');
+	clearElement(treeDiv);
+
+	var table = document.createElement('table');
+	table.setAttribute('style', 'width: 100%;');
+
+	var tableRow = document.createElement('tr');
+	tableRow.setAttribute('style', 'border-bottom: solid black 1px;');
+
+	var typeTableHeader = document.createElement('th');
+	typeTableHeader.setAttribute('style', 'padding-right: 50px; width: 15%; border-right: solid black 1px;');
+	typeTableHeader.innerHTML = 'Type';
+	tableRow.appendChild(typeTableHeader);
+
+	var identifierTableHeader = document.createElement('th');
+	identifierTableHeader.setAttribute('style', 'padding-right: 50px; width: 15%; border-right: solid black 1px;');
+	identifierTableHeader.innerHTML = 'Identifier';
+	tableRow.appendChild(identifierTableHeader);
+
+	var attributeTableHeader = document.createElement('th');
+	attributeTableHeader.setAttribute('style', 'padding-right: 50px; width: 30%; border-right: solid black 1px;');
+	attributeTableHeader.innerHTML = 'Attribute';
+	tableRow.appendChild(attributeTableHeader);
+
+	var valueTableHeader = document.createElement('th');
+	valueTableHeader.setAttribute('style', 'padding-right: 50px; border-right: solid black 1px;');
+	valueTableHeader.innerHTML = 'Value';
+	tableRow.appendChild(valueTableHeader);
+
+	var actionTableHeader = document.createElement('th');
+	actionTableHeader.setAttribute('style', 'width: 5%;');
+	tableRow.appendChild(actionTableHeader);
+
+	table.appendChild(tableRow);
+
+	for(var siteCount = 0; siteCount < data.length; siteCount++) {
+		var site = data[siteCount];
+		displayAttributes('Site', site.SiteName, site.Attributes, table);
+
+		for(var meterCount = 0; meterCount < site.Meters.length; meterCount++) {
+			var meter = site.Meters[meterCount];
+			displayAttributes('Meter', meter.Identifier, meter.Attributes, table);
+
+			if(meter.SubMeters) {
+				for(var subMeterCount = 0; subMeterCount < meter.SubMeters.length; subMeterCount++) {
+					var subMeter = meter.SubMeters[subMeterCount];
+					displayAttributes('Submeter', subMeter.Identifier, subMeter.Attributes, table);
+				}
+			}
+		}
+	}
+
+	treeDiv.appendChild(table);
+}
+
+function displayAttributes(type, identifier, attributes, table) {
+	if(!attributes) {
+		return;
+	}
+
+	for(var i = 0; i < attributes.length; i++) {
+		var tableRow = document.createElement('tr');
+
+		for(var j = 0; j < 4; j++) {
+			var tableDatacell = document.createElement('td');
+			tableDatacell.setAttribute('style', 'border-right: solid black 1px;');
+
+			switch(j) {
+				case 0:
+					tableDatacell.innerHTML = type;
+					break;	
+				case 1:
+					tableDatacell.innerHTML = identifier;
+					break;
+				case 2:
+					for(var key in attributes[i]) {
+						tableDatacell.innerHTML = key;
+						break;
+					}					
+					break;
+				case 3:
+					for(var key in attributes[i]) {
+						tableDatacell.innerHTML = attributes[i][key];
+						break;
+					}
+					break;
+			}
+
+			tableRow.appendChild(tableDatacell);
+		}
+
+		var tableDatacell = document.createElement('td');
+
+		var editIcon = document.createElement('i');
+		editIcon.setAttribute('class', 'fas fa-edit');
+
+		var deleteIcon = document.createElement('i');
+		deleteIcon.setAttribute('class', 'fas fa-trash-alt');
+
+		tableDatacell.appendChild(editIcon);
+		tableDatacell.appendChild(deleteIcon);
+
+		tableRow.appendChild(tableDatacell);
+
+		table.appendChild(tableRow);
+	}	
+}
