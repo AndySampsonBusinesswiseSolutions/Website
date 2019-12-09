a1c36351ffeca7a351075a286a3129c835c5ddad
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2019-12-09 12:53:15 +0000

    3-portfolio-management-site-management display-attributes
    Add connection to google maps

--- UI/portfoliomanagement/sitemanagement.php
+++ UI/portfoliomanagement/sitemanagement.php
@@ -19,7 +19,9 @@
 <body>
 	<div class="section-header section-header-text"><?php echo $PAGE_TITLE ?></div>
 
-	<div class="row">
+	
+
+	<div class="row"> -->
 		<div class="tree-column">
 			<div>
 				<br>
@@ -34,23 +36,27 @@
 			<br>
 			<div>
 				<div class="group-by-div">
-					<div style="overflow-y:auto;" class="datagrid" id="displayAttributes">
-					</div>
+				<div id="map-canvas" style="width: 250px; height: 250px;"></div>
+					<!-- <div style="overflow-y:auto;" class="datagrid" id="displayAttributes">
+					</div> -->
 				</div>
 			</div>
-		</div>	
+		</div>
 	</div>
 	<br>
 </body>
 
 <script src="/javascript/utils.js"></script>
 <script src="/javascript/tree.js"></script>
+<script src="/javascript/map.js"></script>
 <script type="text/javascript" src="/basedata/data.json"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEzl4cfd2OyotR5jHTowAoxwRzOyX8jws"
+  type="text/javascript"></script>
 
 <script type="text/javascript"> 
 	createTree(data, "Hierarchy", "treeDiv", "", "");
 	addExpanderOnClickEvents();
-	buildDataTable();
+	// buildDataTable();
 
 	window.onload = function(){
 		resizeFinalColumns(365);
@@ -59,6 +65,8 @@
 	window.onresize = function(){
 		resizeFinalColumns(365);
 	}
+
+	initializeMap('BB9 5SR');
 </script>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
