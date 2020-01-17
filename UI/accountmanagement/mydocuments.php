a15f0a70633933b1f9e4a3105773761fa0fa018c
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-01-17 11:15:10 +0000

    6-account-management-my-documents
    initial changes

--- UI/accountmanagement/mydocuments.php
+++ UI/accountmanagement/mydocuments.php
@@ -4,11 +4,60 @@
 	include($_SERVER['DOCUMENT_ROOT']."/includes/navigation.php");
 ?>
 
+<link rel="stylesheet" href="/css/tree.css">
+
 <body>
 	<div class="section-header section-header-text"><?php echo $PAGE_TITLE ?></div>
 
-	<div class="section-body">
 	<div>
+		<div id="loaHeader">
+			<div style="padding: 15px;">
+				<div class="datagrid" style="height: 100px;">
+					Your current LOA details:
+					<br><span style="font-size: 20px; color: red;">Your current LOA is 5 days away from expiring!</span>
+					<br><span>Here we can put any other info about the current LOA or anything we so desire</span>
+				</div>
+			</div>
+		<div>
+	</div>	
+	<div class="row">
+		<div class="tree-column">
+			<div>
+				<div class="tree-column">
+					<div id="treeDiv" class="tree-div">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="fill-column"></div>
+		<div class="final-column">
+			<div>
+				<div class="group-by-div" id="cardDiv" style="display: none;">
+					<div class="tabDiv" id="tabDiv" style="overflow-y: auto; overflow: auto;"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
 </body>
+
+<script src="/javascript/utils.js"></script>
+<script src="/javascript/documenttree.js"></script>
+<script src="/javascript/documenttab.js"></script>
+<script type="text/javascript" src="/basedata/document.json"></script>
+
+<script type="text/javascript"> 
+	var data = documents;
+	createTree(data, "treeDiv", "createCardButton");
+	addExpanderOnClickEvents();	
+
+	window.onload = function(){
+		resizeFinalColumns(380);
+	}
+
+	window.onresize = function(){
+		resizeFinalColumns(380);
+	}
+</script>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
---
+++ UI/javascript/documenttab.js
@@ -0,0 +1,165 @@
+function openTab(evt, tabName, guid, branch) {
+	var cardDiv = document.getElementById('cardDiv');
+
+	var tabContent = document.getElementsByClassName("tabcontent");
+	var tabContentLength = tabContent.length;
+	for (var i = 0; i < tabContentLength; i++) {
+	  cardDiv.removeChild(tabContent[i]);
+	}
+
+	var tabLinks = document.getElementsByClassName("tablinks");
+	var tabLinksLength = tabLinks.length;
+	for (var i = 0; i < tabLinksLength; i++) {
+		tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+	}
+	
+	var newDiv = document.createElement('div');
+	newDiv.setAttribute('class', 'tabcontent');
+	newDiv.id = tabName;
+	cardDiv.appendChild(newDiv);
+
+	createCard(guid, newDiv, 'Document');
+
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
+
+function createCardButton(checkbox){
+	var cardDiv = document.getElementById('cardDiv');
+	var tabDiv = document.getElementById('tabDiv');
+	var span = document.getElementById(checkbox.id.replace('checkbox', 'span'));
+
+	if(checkbox.checked){
+		cardDiv.setAttribute('style', '');
+		var button = document.createElement('button');
+		button.setAttribute('class', 'tablinks');
+		button.setAttribute('onclick', 'openTab(event, "' + span.id.replace('span', 'div') +'", "' + checkbox.getAttribute('guid') + '", "' + checkbox.getAttribute('branch') + '")');
+
+		button.innerHTML = span.innerHTML;
+		button.id = span.id.replace('span', 'button');
+		tabDiv.appendChild(button);
+	
+		updateTabDiv();
+	}
+	else {
+		tabDiv.removeChild(document.getElementById(span.id.replace('span', 'button')));
+
+		var divToRemove = document.getElementById(span.id.replace('span', 'div'));
+		if(divToRemove) {
+			cardDiv.removeChild();
+		}
+
+		if(tabDiv.children.length == 0) {
+			cardDiv.setAttribute('style', 'display: none;');
+		}
+		else {
+            updateTabDiv();
+		}
+	}	
+}
+
+function updateTabDiv() {
+	var tabDiv = document.getElementById('tabDiv');
+	var tabDivChildren = tabDiv.children;
+	var tabDivChildrenLength = tabDivChildren.length;
+
+    tabDivChildren[0].setAttribute('style', 'width: '.concat(tabDiv.clientWidth/tabDivChildrenLength).concat('px;'));
+    for(var i = 1; i < tabDivChildrenLength; i++) {
+        tabDivChildren[i].setAttribute('style', 'width: '.concat(tabDiv.clientWidth/tabDivChildrenLength).concat('px; border-left: solid black 1px;'));
+    }
+}
+
+function createCard(guid, divToAppendTo, identifier) {
+	var document;
+	
+	var documentLength = data.length;
+	for(var i = 0; i < documentLength; i++) {
+		document = data[i];
+
+		if(document.GUID == guid) {
+			break;
+		}
+	}
+
+	buildDocumentDataTable(document, identifier, divToAppendTo);
+}
+
+function buildDocumentDataTable(entity, attributeRequired, divToAppendTo){
+	var div = document.createElement('div');
+	div.id = 'displayAttributes';
+	divToAppendTo.appendChild(div);
+	
+	var treeDiv = document.getElementById('displayAttributes');
+	clearElement(treeDiv);
+
+	var table = document.createElement('table');
+	table.id = 'dataTable';
+	table.setAttribute('style', 'width: 100%;');
+
+	var tableRow = document.createElement('tr');
+
+	tableRow.appendChild(createTableHeader('width: 15%; border: solid black 1px;', 'Document'));
+	tableRow.appendChild(createTableHeader('width: 30%; border: solid black 1px;', 'Upload Date'));
+	tableRow.appendChild(createTableHeader('border: solid black 1px;', 'Uploaded By'));
+	tableRow.appendChild(createTableHeader('width: 5%; border: solid black 1px;', 'Download'));
+
+    table.appendChild(tableRow);
+	displayAttributes(getAttribute(entity.Attributes, attributeRequired), entity.Attributes, table, 'Document');
+
+	treeDiv.appendChild(table);
+}
+
+function displayAttributes(identifier, attributes, table, type) {
+	if(!attributes) {
+		return;
+	}
+
+	var attributesLength = attributes.length;
+	for(var i = 0; i < attributesLength; i++) {
+		var tableRow = document.createElement('tr');
+		tableRow.id = 'row'.concat(type + i);
+
+		for(var j = 0; j < 4; j++) {
+			var tableDatacell = document.createElement('td');
+			tableDatacell.setAttribute('style', 'border: solid black 1px;');
+
+			switch(j) {
+				case 0:
+					tableDatacell.innerHTML = type;
+					break;	
+				case 1:
+					tableDatacell.innerHTML = identifier;
+					break;
+				case 2:
+					for(var key in attributes[i]) {
+						tableDatacell.innerHTML = key;
+						break;
+					}	
+					
+					tableDatacell.id = 'attribute'.concat(type + i);
+					break;
+				case 3:
+					tableDatacell.id = 'value'.concat(type + i);
+
+					var downloadIcon = document.createElement('i');
+					downloadIcon.setAttribute('class', 'fas fa-download');
+					downloadIcon.setAttribute('title', 'Download Now');
+					downloadIcon.setAttribute('style', 'padding-right: 15px;');
+
+					var addToDownloadBasketIcon = document.createElement('i');
+					addToDownloadBasketIcon.setAttribute('class', 'fas fa-cart-arrow-down');
+					addToDownloadBasketIcon.setAttribute('title', 'Add To Download Cart');
+
+					tableDatacell.appendChild(downloadIcon);
+					tableDatacell.appendChild(addToDownloadBasketIcon);
+					break;
+			}
+
+			tableRow.appendChild(tableDatacell);
+		}
+
+		tableRow.appendChild(tableDatacell);
+
+		table.appendChild(tableRow);
+	}	
+}
