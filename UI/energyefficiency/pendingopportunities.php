b190463c5a4cd8360170b8047d2b5756ac8c385e
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-02-07 14:34:21 +0000

    31-energy-efficiency-opportunities
    Initial set up of pending opportunities

--- UI/energyefficiency/pendingopportunities.php
+++ UI/energyefficiency/pendingopportunities.php
@@ -6,9 +6,37 @@
 
 <body>
 	<div class="section-header section-header-text"><?php echo $PAGE_TITLE ?></div>
-
-	<div class="section-body">
-	<div>
+	<br>
+	<div id="tableContainer" class="tableContainer3" style="padding-left: 15px; padding-right: 15px;">
+		<table style="width: 100%;">
+			<thead>
+				<tr>
+					<th id="th1" style="border: solid black 1px;">Project Name</th>
+					<th id="th2" style="border: solid black 1px;">Site</th>
+					<th id="th3" style="border: solid black 1px;">Meter</th>
+					<th id="th4" style="border: solid black 1px;">Engineer</th>
+					<th id="th5" style="border: solid black 1px;">Estimated Start Date</th>
+					<th id="th6" style="border: solid black 1px;">Estimated Finish Date</th>
+					<th id="th7" style="border: solid black 1px;">Estimated Cost</th>
+					<th id="th8" style="border: solid black 1px;">Estimated kWh Savings (pa)</th>
+					<th id="th9" style="border: solid black 1px;">Estimated £ Savings (pa)</th>
+				</tr>
+			</thead>
+			<tbody class="scrollContent3">
+				<tr>
+					<td headers="th1" style="border: solid black 1px;">LED Lighting</td>
+					<td headers="th2" style="border: solid black 1px;">Site X</td>
+					<td headers="th3" style="border: solid black 1px;">N/A</td>
+					<td headers="th4" style="border: solid black 1px;">En Gineer</td>
+					<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
+					<td headers="th6" style="border: solid black 1px;">30/06/2020</td>
+					<td headers="th7" style="border: solid black 1px;">£100,000</td>
+					<td headers="th8" style="border: solid black 1px;">10,000</td>
+					<td headers="th9" style="border: solid black 1px;">£15,000</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </body>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
