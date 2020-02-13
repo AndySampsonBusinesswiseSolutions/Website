31dffe876bc3acc78dbc85594a4e5d362df2bdd0
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-02-13 14:18:06 +0000

    31-energy-efficiency-opportunities
    updates to Opportunity Management page

--- UI/energyefficiency/opportunitymanagement.php
+++ UI/energyefficiency/opportunitymanagement.php
@@ -22,7 +22,7 @@
 		<div class="final-column">
 			<div class="row">
 				<div class="divcolumn first"></div>
-				<div class="divcolumn left" style="border: solid black 1px">
+				<div class="divcolumn left" style="border: solid black 1px; background-color: #e9eaee;">
 					<div class="divcolumn first"></div>
 					<div class="divcolumn" style="width: 96%;">
 						<div style="text-align: center; border-bottom: solid black 1px;">
@@ -36,7 +36,7 @@
 										<th id="th1" style="border: solid black 1px;">Customer</th>
 										<th id="th2" style="border: solid black 1px;">Site</th>
 										<th id="th5" style="border: solid black 1px;">Requested Date</th>
-										<th id="th10" style="border: solid black 1px;"></th>
+										<th id="th10" style="border: solid black 1px;" colspan="2">Manage Visit</th>
 									</tr>
 								</thead>
 								<tbody class="scrollContent3">
@@ -44,7 +44,8 @@
 										<td headers="th1" style="border: solid black 1px;">David Ford Trading Ltd</td>
 										<td headers="th2" style="border: solid black 1px;">Site X</td>
 										<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
-										<td headers="th10" style="border: solid black 1px;"><button>Schedule Visit</button></td>
+										<td headers="th10" style="border: solid black 1px;"><button class="show-pointer" onclick="scheduleVisit(1)">Schedule Visit</button></td>
+										<td headers="th11" style="border: solid black 1px;"><button class="show-pointer" onclick="rejectVisit(1)">Reject Visit</button></td>
 									</tr>
 								</tbody>
 							</table>
@@ -53,11 +54,11 @@
 					<div class="divcolumn last"></div>
 				</div>
 				<div class="divcolumn middle"></div>
-				<div class="divcolumn right" style="border: solid black 1px">
+				<div class="divcolumn right" style="border: solid black 1px; background-color: #e9eaee;">
 					<div class="divcolumn first"></div>
 					<div class="divcolumn" style="width: 96%;">
 						<div style="text-align: center; border-bottom: solid black 1px;">
-							<span>Future Visits</span>
+							<span>Scheduled Visits</span>
 						</div>
 						<br>
 						<div id="tableContainer" class="tableContainer3">
@@ -67,10 +68,9 @@
 										<th id="th0" style="border: solid black 1px;">Customer</th>
 										<th id="th2" style="border: solid black 1px;">Site</th>
 										<th id="th3" style="border: solid black 1px;">Meter</th>
-										<th id="th1" style="border: solid black 1px;">Project<br>Name</th>
 										<th id="th4" style="border: solid black 1px;">Engineer</th>
 										<th id="th5" style="border: solid black 1px;">Visit<br>Date</th>
-										<th id="th10" style="border: solid black 1px;"></th>
+										<th id="th10" style="border: solid black 1px;" colspan="2">Manage Visit</th>
 									</tr>
 								</thead>
 								<tbody class="scrollContent3">
@@ -78,10 +78,10 @@
 										<td headers="th0" style="border: solid black 1px;">David Ford Trading Ltd</td>
 										<td headers="th2" style="border: solid black 1px;">Site X</td>
 										<td headers="th3" style="border: solid black 1px;">N/A</td>
-										<td headers="th1" style="border: solid black 1px;">LED Lighting</td>
 										<td headers="th4" style="border: solid black 1px;">En Gineer</td>
 										<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
-										<td headers="th10" style="border: solid black 1px;"><button>Start Visit</button></td>
+										<td headers="th10" style="border: solid black 1px;"><button class="show-pointer">Start Visit</button></td>
+										<td headers="th11" style="border: solid black 1px;"><button class="show-pointer" onclick="rejectVisit(1)">Reject Visit</button></td>
 									</tr>
 								</tbody>
 							</table>
@@ -94,7 +94,7 @@
 			<br>
 			<div class="row">
 				<div class="divcolumn first"></div>
-				<div style="border: solid black 1px; width: 96%;">
+				<div style="border: solid black 1px; width: 96%; background-color: #e9eaee;">
 					<div class="divcolumn first"></div>
 					<div class="divcolumn" style="width: 96%;">
 						<div style="text-align: center; border-bottom: solid black 1px;">
@@ -105,6 +105,7 @@
 							<table style="width: 100%;">
 								<thead>
 									<tr>
+										<th id="th0" style="border: solid black 1px;">Customer</th>
 										<th id="th1" style="border: solid black 1px;">Project<br>Name</th>
 										<th id="th2" style="border: solid black 1px;">Site</th>
 										<th id="th3" style="border: solid black 1px;">Meter</th>
@@ -120,6 +121,7 @@
 								</thead>
 								<tbody class="scrollContent3">
 									<tr>
+										<td headers="th0" style="border: solid black 1px;">David Ford Trading Ltd</td>
 										<td headers="th1" style="border: solid black 1px;">LED Lighting</td>
 										<td headers="th2" style="border: solid black 1px;">Site X</td>
 										<td headers="th3" style="border: solid black 1px;">N/A</td>
@@ -130,9 +132,9 @@
 										<td headers="th7" style="border: solid black 1px;">£100,000</td>
 										<td headers="th8" style="border: solid black 1px;">10,000</td>
 										<td headers="th9" style="border: solid black 1px;">£15,000</td>
-										<td headers="th11" style="border: solid black 1px;"><button>Close Project</button></td>
-										<td headers="th12" style="border: solid black 1px;"><button>Manage Project Details</button></td>
-										<td headers="th13" style="border: solid black 1px;"><button>Manage Project Timelines</button></td>
+										<td headers="th11" style="border: solid black 1px;"><button class="show-pointer">Close Project</button></td>
+										<td headers="th12" style="border: solid black 1px;"><button class="show-pointer">Manage Project Details</button></td>
+										<td headers="th13" style="border: solid black 1px;"><button class="show-pointer">Manage Project Timelines</button></td>
 									</tr>
 								</tbody>
 							</table>
@@ -148,6 +150,8 @@
 </body>
 
 <script src="/javascript/utils.js"></script>
+<link href="https://cdn.jsdelivr.net/gh/xxjapp/xdialog@3/xdialog.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/gh/xxjapp/xdialog@3/xdialog.min.js"></script>
 
 <script type="text/javascript"> 
 	window.onload = function(){
@@ -157,6 +161,51 @@
 	window.onresize = function(){
 		resizeFinalColumns(375);
 	}
-	</script>
+</script>
+
+<script>
+	function scheduleVisit(row) {
+		var table = document.createElement('table');
+		table.setAttribute('style', 'width: 100%;');
+
+		var tableRow = document.createElement('tr');
+		tableRow.appendChild(createTableHeader('border: solid black 1px;', 'Rate'));
+		tableRow.appendChild(createTableHeader('border: solid black 1px;', 'Value'));
+		table.appendChild(tableRow);
+
+			xdialog.confirm(table.outerHTML, function() {
+			}, {
+				style: 'width:420px;font-size:0.8rem;',
+				buttons: {
+					ok: {
+						text: 'Schedule',
+						style: 'background: green;',
+					},
+					cancel: {
+						text: 'Cancel',
+						style: 'background: grey;',
+					}
+				},
+			title: 'Schedule Visit - Requested Date 01/04/2020',
+			});
+	}
+
+	function rejectVisit(row) {
+		var confirmRejectionDialog = xdialog.confirm('Reject Customer Visit?', function() {
+		}, {
+			style: 'width:420px;font-size:0.8rem;',
+			buttons: {
+				ok: {
+					text: 'Yes',
+					style: 'background: red;',
+				},
+				cancel: {
+					text: 'No',
+					style: 'background: green;',
+				}
+			}
+		});
+	}
+</script>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
