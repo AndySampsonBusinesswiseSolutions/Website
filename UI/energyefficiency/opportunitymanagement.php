e93ceabf319fbe5c019d2c0f8969ad68575abec2
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-02-14 11:50:05 +0000

    31-energy-efficiency-opportunities
    update to get SiteVisit page to load

--- UI/energyefficiency/opportunitymanagement.php
+++ UI/energyefficiency/opportunitymanagement.php
@@ -69,7 +69,7 @@
 										<th id="th2" style="border: solid black 1px;">Site</th>
 										<th id="th3" style="border: solid black 1px;">Meter</th>
 										<th id="th4" style="border: solid black 1px;">Engineer</th>
-										<th id="th5" style="border: solid black 1px;">Visit<br>Date</th>
+										<th id="th5" style="border: solid black 1px;">Visit Date</th>
 										<th id="th10" style="border: solid black 1px;" colspan="2">Manage Visit</th>
 									</tr>
 								</thead>
@@ -80,7 +80,7 @@
 										<td headers="th3" style="border: solid black 1px;">N/A</td>
 										<td headers="th4" style="border: solid black 1px;">En Gineer</td>
 										<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
-										<td headers="th10" style="border: solid black 1px;"><button class="show-pointer">Start Visit</button></td>
+										<td headers="th10" style="border: solid black 1px;"><button class="show-pointer" onclick="getConfirmation()">Start Visit</button></td>
 										<td headers="th11" style="border: solid black 1px;"><button class="show-pointer" onclick="rejectVisit(1)">Reject Visit</button></td>
 									</tr>
 								</tbody>
@@ -91,6 +91,58 @@
 				</div>
 				<div class="divcolumn last"></div>
 			</div>
+			<br>
+			<div class="row">
+				<div class="divcolumn first"></div>
+				<div style="border: solid black 1px; width: 96%; background-color: #e9eaee;">
+					<div class="divcolumn first"></div>
+					<div class="divcolumn" style="width: 96%;">
+						<div style="text-align: center; border-bottom: solid black 1px;">
+							<span>Recommended Opportunities</span>
+						</div>
+						<br>
+						<div id="tableContainer" class="tableContainer3">
+							<table style="width: 100%;">
+								<thead>
+									<tr>
+										<th id="th0" style="border: solid black 1px;">Customer</th>
+										<th id="th1" style="border: solid black 1px;">Project<br>Name</th>
+										<th id="th2" style="border: solid black 1px;">Site</th>
+										<th id="th3" style="border: solid black 1px;">Meter</th>
+										<th id="th10" style="border: solid black 1px;">Notes</th>
+										<th id="th4" style="border: solid black 1px;">Engineer</th>
+										<th id="th5" style="border: solid black 1px;">Estimated<br>Start Date</th>
+										<th id="th6" style="border: solid black 1px;">Estimated<br>Finish Date</th>
+										<th id="th7" style="border: solid black 1px;">Estimated<br>Cost</th>
+										<th id="th8" style="border: solid black 1px;">Estimated<br>kWh Savings (pa)</th>
+										<th id="th9" style="border: solid black 1px;">Estimated<br>£ Savings (pa)</th>
+										<th id="th11" style="border: solid black 1px;" colspan="2">Manage Project</th>
+									</tr>
+								</thead>
+								<tbody class="scrollContent3">
+									<tr>
+										<td headers="th0" style="border: solid black 1px;">David Ford Trading Ltd</td>
+										<td headers="th1" style="border: solid black 1px;">LED Lighting</td>
+										<td headers="th2" style="border: solid black 1px;">Site X</td>
+										<td headers="th3" style="border: solid black 1px;">N/A</td>
+										<td headers="th10" style="border: solid black 1px;"><i class="fas fa-search show-pointer"></i></td>
+										<td headers="th4" style="border: solid black 1px;">En Gineer</td>
+										<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
+										<td headers="th6" style="border: solid black 1px;">30/06/2020</td>
+										<td headers="th7" style="border: solid black 1px;">£100,000</td>
+										<td headers="th8" style="border: solid black 1px;">10,000</td>
+										<td headers="th9" style="border: solid black 1px;">£15,000</td>
+										<td headers="th11" style="border: solid black 1px;"><button class="show-pointer">Approve Project</button></td>
+										<td headers="th12" style="border: solid black 1px;"><button class="show-pointer">Reject Project</button></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class="divcolumn last"></div>
+				</div>
+				<div class="divcolumn last"></div>
+			</div>	
 			<br>
 			<div class="row">
 				<div class="divcolumn first"></div>
@@ -206,6 +258,13 @@
 			}
 		});
 	}
+
+	function getConfirmation() {
+               var retVal = confirm("Do you want to continue ?");
+               if( retVal == true ) {
+                  window.location = '../SiteVisit'
+               }
+            }
 </script>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
---
+++ UI/energyefficiency/sitevisit.php
@@ -0,0 +1,11 @@
+<?php 
+	$PAGE_TITLE = "Site Visit";
+	include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");
+	include($_SERVER['DOCUMENT_ROOT']."/includes/navigation.php");
+?>
+
+<body>
+    <div class="section-header section-header-text"><?php echo $PAGE_TITLE ?></div>
+</body>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
