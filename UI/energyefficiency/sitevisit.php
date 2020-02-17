7c0c01316144811963d4bd71fa54beffaed061d1
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-02-17 10:57:53 +0000

    31-energy-efficiency-opportunities
    basic outline of SiteVisit

--- UI/energyefficiency/sitevisit.php
+++ UI/energyefficiency/sitevisit.php
@@ -6,6 +6,21 @@
 
 <body>
     <div class="section-header section-header-text"><?php echo $PAGE_TITLE ?></div>
+    <br>
+    <div class="row" style="padding-left: 15px; padding-right: 15px;">
+        <div class="divcolumn first"></div>
+        <div style="border: solid black 1px;" class="divcolumn left"></div>
+        <div class="divcolumn middle"></div>
+        <div style="border: solid black 1px;" class="divcolumn right"></div>
+        <div class="divcolumn last"></div>
+    </div>
+    <br>
+    <div class="row" style="padding-left: 15px; padding-right: 15px;">
+        <div class="divcolumn first"></div>
+        <div style="border: solid black 1px; width: 96%;"></div>
+        <div class="divcolumn last"></div>
+    </div>
+    <br>
 </body>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
