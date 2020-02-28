da95e5c3b9327e0e79a105fee9656e2447272d95
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-02-28 16:23:55 +0000

    0-portal-project
    Redesign Supplier Product Management page so it has its own folder

--- UI/internal/suppliermanagement/supplierproductmanagement/supplierproductmanagement.js
+++ UI/internal/suppliermanagement/supplierproductmanagement/supplierproductmanagement.js
@@ -1,3 +1,8 @@
+function pageLoad() {
+	createTree(supplierproduct, "treeDiv", "createCardButton");
+	addExpanderOnClickEvents();
+}
+
 function openTab(evt, tabName, guid, branch) {
 	var cardDiv = document.getElementById('cardDiv');
 
@@ -78,7 +83,7 @@ function createCard(guid, divToAppendTo, identifier) {
 	
 	var supplierproductLength = supplierproduct.length;
 	for(var i = 0; i < supplierproductLength; i++) {
-		var site = data[i];
+		var site = supplierproduct[i];
 		var commodities = site.Commodities;
 		
 		var commodityLength = commodities.length;
@@ -133,7 +138,12 @@ function buildCardView(entity, divToAppendTo){
 	table.appendChild(createTableHeader('width: 50%', ''));
 	table.appendChild(createTableHeader('width: 50%', ''));
 
-	var cardViewAttributes = siteCardViewAttributes;
+	var cardViewAttributes = [
+		"ProductName",
+		"DayPeriod",
+		"NightPeriod",
+		"Evening&WeekendPeriod"
+	];
 	var cardViewAttributesLength = cardViewAttributes.length;
 	var entityAttributes = entity.Attributes;
 
@@ -387,4 +397,360 @@ function cancelRow(row) {
 	showHideIcon('saveRow' + row, 'display: none;');
 	showHideIcon('undoRow' + row, 'display: none;');
 	showHideIcon('cancelRow' + row, 'display: none;');
+}
+
+var branchCount = 0;
+var subBranchCount = 0;
+
+function createTree(baseData, divId, checkboxFunction) {
+    var tree = document.createElement('div');
+    tree.setAttribute('class', 'scrolling-wrapper');
+    
+    var ul = createUL();
+    tree.appendChild(ul);
+
+    branchCount = 0;
+    subBranchCount = 0; 
+
+    buildTree(baseData, ul, checkboxFunction);
+
+    var div = document.getElementById(divId);
+    clearElement(div);
+    div.appendChild(tree);
+}
+
+function buildTree(baseData, baseElement, checkboxFunction) {
+    var dataLength = baseData.length;
+    for(var i = 0; i < dataLength; i++){
+        var base = baseData[i];
+        var baseName = getAttribute(base.Attributes, 'BaseName');
+        var li = document.createElement('li');
+        var ul = createUL();
+
+        buildCommodity(base.Commodities, ul, checkboxFunction, baseName);
+        appendListItemChildren(li, 'Site'.concat(base.GUID), checkboxFunction, 'Site', baseName, ul, baseName, base.GUID);
+
+        baseElement.appendChild(li);        
+    }
+}
+
+function buildCommodity(commodities, baseElement, checkboxFunction, linkedSite) {
+    var commoditiesLength = commodities.length;
+    for(var i = 0; i < commoditiesLength; i++) {
+        var commodity = commodities[i];
+        var li = document.createElement('li');
+
+        var ul = createUL();
+        buildProfileClass(commodity.ProfileClasses, ul, checkboxFunction, linkedSite);
+        appendListItemChildren(li, 'Commodity'.concat(branchCount), checkboxFunction, 'Commodity', commodity.CommodityName, ul, linkedSite, '');
+
+        baseElement.appendChild(li);
+        branchCount++;
+    }
+}
+
+function buildProfileClass(profileClasses, baseElement, checkboxFunction, linkedSite) {
+    var profileClassesLength = profileClasses.length;
+    for(var i = 0; i < profileClassesLength; i++) {
+        var profileClass = profileClasses[i];
+        var li = document.createElement('li');
+        var ul = createUL();
+        buildProduct(profileClass.Products, ul, checkboxFunction, linkedSite);
+        appendListItemChildren(li, 'ProfileClass'.concat(subBranchCount), checkboxFunction, 'ProfileClass', profileClass.MeterType, ul, linkedSite, '');
+
+        baseElement.appendChild(li);
+        subBranchCount++;
+    }
+}
+
+function appendListItemChildren(li, id, checkboxFunction, checkboxBranch, branchOption, ul, linkedSite, guid) {
+    li.appendChild(createBranchDiv(id));
+    li.appendChild(createTreeIcon(branchOption));
+    li.appendChild(createSpan(id, branchOption));
+    li.appendChild(createBranchListDiv(id.concat('List'), ul));
+}
+
+function buildProduct(products, baseElement, checkboxFunction, linkedSite) {
+    var productsLength = products.length;
+    for(var i = 0; i < productsLength; i++){
+        var product = products[i];
+        var productAttributes = product.Attributes;
+        var identifier = getAttribute(productAttributes, 'ProductName');
+        var li = document.createElement('li');
+        var branchId = 'Meter'.concat(product.GUID);
+        var branchDiv = createBranchDiv(branchId);
+        
+        branchDiv.removeAttribute('class', 'far fa-plus-square');
+        branchDiv.setAttribute('class', 'far fa-times-circle');
+
+        li.appendChild(branchDiv);
+        li.appendChild(createCheckbox(branchId, checkboxFunction, 'Product', linkedSite, product.GUID));
+        li.appendChild(createTreeIcon('Product'));
+        li.appendChild(createSpan(branchId, identifier));
+
+        baseElement.appendChild(li); 
+    }
+}
+
+function createBranchDiv(branchDivId) {
+    var branchDiv = document.createElement('div');
+    branchDiv.id = branchDivId;
+    branchDiv.setAttribute('class', 'far fa-plus-square');
+    branchDiv.setAttribute('style', 'padding-right: 4px;');
+    return branchDiv;
+}
+
+function createBranchListDiv(branchListDivId, ul) {
+    var branchListDiv = document.createElement('div');
+    branchListDiv.id = branchListDivId;
+    branchListDiv.setAttribute('class', 'listitem-hidden');
+    branchListDiv.appendChild(ul);
+    return branchListDiv;
+}
+
+function createUL() {
+    var ul = document.createElement('ul');
+    ul.setAttribute('class', 'format-listitem');
+    return ul;
+}
+
+function createTreeIcon(branch) {
+    var icon = document.createElement('i');
+    icon.setAttribute('class', getIconByBranch(branch));
+    icon.setAttribute('style', 'padding-left: 3px; padding-right: 3px;');
+    return icon;
+}
+
+function createSpan(spanId, innerHTML) {
+    var span = document.createElement('span');
+    span.id = spanId.concat('span');
+    span.innerHTML = innerHTML;
+    return span;
+}
+
+function createCheckbox(checkboxId, checkboxFunction, branch, linkedSite, guid) {
+    var functionArray = checkboxFunction.replace(')', '').split('(');
+    var functionArrayLength = functionArray.length;
+    var functionName = functionArray[0];
+    var functionArguments = [];
+
+    var checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';  
+    checkBox.id = checkboxId.concat('checkbox');
+    checkBox.setAttribute('Branch', branch);
+    checkBox.setAttribute('LinkedSite', linkedSite);
+    checkBox.setAttribute('GUID', guid);
+
+    functionArguments.push(checkBox.id);
+    if(functionArrayLength > 1) {
+        var functionArgumentLength = functionArray[1].split(',').length;
+        for(var i = 0; i < functionArgumentLength; i++) {
+            functionArguments.push(functionArray[1].split(',')[i]);
+        }
+    }
+    functionName = functionName.concat('(').concat(functionArguments.join(',').concat(')'));
+    
+    checkBox.setAttribute('onclick', functionName);
+    return checkBox;
+}
+
+function getIconByBranch(branch) {
+    switch (branch) {
+        case 'Unknown':
+            return 'fas fa-question-circle';
+        default:
+            return 'fas fa-map-marker-alt';
+    }
+}
+
+function clearElement(element) {
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
+}
+
+function getAttribute(attributes, attributeRequired) {
+	for (var attribute in attributes) {
+		var array = attributes[attribute];
+
+		for(var key in array) {
+			if(key == attributeRequired) {
+				return array[key];
+			}
+		}
+	}
+
+	return null;
+}
+
+function getEntityByGUID(guid, type) {
+	var dataLength = data.length;
+	for(var i = 0; i < dataLength; i++) {
+		var site = data[i];
+		if(type == 'Site' || type == 'Supplier') {
+			if(site.GUID == guid) {
+				return site;
+			}
+		}
+        else {
+			var meterLength = site.Meters.length;
+			for(var j = 0; j < meterLength; j++) {
+				var meter = site.Meters[j];
+				if(type = 'Meter') {
+					if(meter.GUID == guid) {
+						return meter;
+					}
+					else {
+						var subMeters = meter.SubMeters;
+						if(subMeters) {
+							var subMetersLength = subMeters.length;
+							for(var k = 0; k < subMetersLength; k++) {
+								var subMeter = subMeters[k];
+								if(subMeter.GUID == guid) {
+									return subMeter;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	return null;
+}
+
+function showHideIcon(iconId, style) {
+	var icon = document.getElementById(iconId);
+	icon.setAttribute('style', style);
+}
+
+function createIcon(iconId, className, style, onClickEvent, title) {
+	var icon = document.createElement('i');
+	icon.id = iconId;
+	icon.setAttribute('class', className);
+
+	if(style) {
+		icon.setAttribute('style', style);
+	}
+
+	if(onClickEvent) {
+		icon.setAttribute('onclick', onClickEvent);
+	}
+
+	if(title) {
+		icon.setAttribute('title', title);
+	}
+
+	return icon;
+}
+
+function createTableHeader(style, value) {
+	var tableHeader = document.createElement('th');
+
+	if(style != '') {
+		tableHeader.setAttribute('style', style);
+	}
+	
+	tableHeader.innerHTML = value;
+	return tableHeader;
+}
+
+function updateClassOnClick(elementId, firstClass, secondClass){
+	var elements = document.getElementsByClassName(elementId);
+
+	if(elements.length == 0) {
+		var element = document.getElementById(elementId);
+		updateClass(element, firstClass, secondClass);
+	}
+	else {
+		for(var i = 0; i< elements.length; i++) {
+			updateClass(elements[i], firstClass, secondClass)
+		}
+	}
+}
+
+function updateClass(element, firstClass, secondClass)
+{
+	if(hasClass(element, firstClass)){
+		element.classList.remove(firstClass);
+
+		if(secondClass != ''){
+			element.classList.add(secondClass);
+		}
+	}
+	else {
+		if(secondClass != ''){
+			element.classList.remove(secondClass);
+		}
+		
+		element.classList.add(firstClass);
+	}
+}
+  
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+function addExpanderOnClickEvents() {
+	var expanders = document.getElementsByClassName('fa-plus-square');
+	var expandersLength = expanders.length;
+	for(var i = 0; i < expandersLength; i++){
+		addExpanderOnClickEventsByElement(expanders[i]);
+	}
+}
+
+function addExpanderOnClickEventsByElement(element) {
+	element.addEventListener('click', function (event) {
+		updateClassOnClick(this.id, 'fa-plus-square', 'fa-minus-square')
+		updateClassOnClick(this.id.concat('List'), 'listitem-hidden', '')
+	});
+
+	updateAdditionalControls(element);
+	expandAdditionalLists(element);
+}
+
+function updateAdditionalControls(element) {
+	var additionalcontrols = element.getAttribute('additionalcontrols');
+
+	if(!additionalcontrols) {
+		return;
+	}
+
+	var listToHide = element.id.concat('List');
+	var clickEventFunction = function (event) {
+		updateClassOnClick(listToHide, 'listitem-hidden', '')
+	};
+
+	var controlArray = additionalcontrols.split(',');
+	for(var j = 0; j < controlArray.length; j++) {
+		var controlId = controlArray[j];	
+
+		element.addEventListener('click', function (event) {
+			var controlElement = document.getElementById(controlId);
+			if(hasClass(this, 'fa-minus-square')) {				
+				controlElement.addEventListener('click', clickEventFunction, false);
+			}
+			else {
+				controlElement.removeEventListener('click', clickEventFunction);
+			}
+		});
+	}	
+}
+
+function expandAdditionalLists(element) {
+	var additionalLists = element.getAttribute('additionallists');
+
+	if(!additionalLists) {
+		return;
+	}
+
+	element.addEventListener('click', function (event) {
+		var controlArray = additionalLists.split(',');
+		for(var j = 0; j < controlArray.length; j++) {
+			var controlId = controlArray[j];
+			var controlElement = document.getElementById(controlId);
+			updateClass(controlElement, 'listitem-hidden', '');
+		}
+	});		
 }
