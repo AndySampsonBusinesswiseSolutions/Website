6be5f981d33868dd30d43fc232a800ba34c5c1a5
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-02-28 14:56:46 +0000

    0-portal-project
    Redesign Manage Users page so it has its own folder

---
+++ UI/internal/accountmanagement/manageusers/index.php
@@ -0,0 +1,50 @@
+<?php 
+	$PAGE_TITLE = "Manage Users";
+	include($_SERVER['DOCUMENT_ROOT']."/includes/navigation/navigation.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $PAGE_TITLE ?></title>
+
+	<link rel="stylesheet" href="manageusers.css">
+</head>
+
+<body>
+	<div class="section-header section-header-text"><?php echo $PAGE_TITLE ?></div>
+
+	<div class="row">
+		<div class="tree-column">
+			<div>
+				<br>
+				<div class="tree-column">
+					<div id="treeDiv" class="tree-div">
+					</div>
+					<button style="margin-top: 5px; margin-bottom: 5px; width: 100%;" onclick='addUser()'>Add New User</button>
+					<button style="margin-bottom: 5px; width: 100%;" onclick='deleteUsers()'>Delete Selected Users</button>
+					<button style="width: 100%;" onclick='reinstateUsers()'>Reinstate Deleted Users</button>
+				</div>
+			</div>
+		</div>
+		<div class="fill-column"></div>
+		<div class="final-column">
+			<br>
+			<div>
+				<div class="group-by-div" id="cardDiv" style="display: none;">
+					<div class="tabDiv" id="tabDiv" style="overflow-y: auto; overflow: auto;"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+</body>
+
+<script type="text/javascript" src="manageusers.js"></script>
+<script type="text/javascript" src="manageusers.json"></script>
+
+<script type="text/javascript"> 
+	pageLoad();
+</script>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer/footer.php");?>
--- UI/internal/accountmanagement/manageusers/manageusers.js
+++ UI/internal/accountmanagement/manageusers/manageusers.js
@@ -1,3 +1,8 @@
+function pageLoad() {
+	createTree(user, "treeDiv", "createCardButton");
+	addExpanderOnClickEvents();
+}
+
 function openTab(evt, tabName, guid, branch) {
 	var cardDiv = document.getElementById('cardDiv');
 
@@ -70,21 +75,21 @@ function updateTabDiv() {
 }
 
 function createCard(guid, divToAppendTo, identifier) {
-	var user;
+	var userEntity;
 	
-	var userLength = data.length;
+	var userLength = user.length;
 	for(var i = 0; i < userLength; i++) {
-		user = data[i];
+		userEntity = user[i];
 
-		if(user.GUID == guid) {
+		if(userEntity.GUID == guid) {
 			break;
 		}
 	}
 
-	buildCardView(user, divToAppendTo);
-	buildUserDataTable(user, identifier, divToAppendTo);
+	buildCardView(userEntity, divToAppendTo);
+	buildUserDataTable(userEntity, identifier, divToAppendTo);
 	divToAppendTo.appendChild(document.createElement('br'));
-	buildRoleDataTable(user, identifier, divToAppendTo);
+	buildRoleDataTable(userEntity, identifier, divToAppendTo);
 }
 
 function buildCardView(entity, divToAppendTo){
@@ -98,7 +103,16 @@ function buildCardView(entity, divToAppendTo){
 	table.appendChild(createTableHeader('width: 50%', ''));
 	table.appendChild(createTableHeader('width: 50%', ''));
 
-	var cardViewAttributes = userCardViewAttributes;
+	var cardViewAttributes = [
+		"Address Line 1",
+		"Address Line 2",
+		"Address Line 3",
+		"Address Line 4",
+		"Postcode",
+		"Contact Name",
+		"Contact Telephone Number",
+		"Email"
+	];
 	var cardViewAttributesLength = cardViewAttributes.length;
 	var entityAttributes = entity.Attributes;
 
@@ -428,4 +442,392 @@ function addRoles(userName) {
 		},
 		title: 'Add Roles For '.concat(userName)
 	});
+}
+
+var branchCount = 0;
+var subBranchCount = 0;
+
+function createTree(baseData, divId, checkboxFunction) {
+    var tree = document.createElement('div');
+    tree.setAttribute('class', 'scrolling-wrapper');
+    
+    var ul = createUL();
+    tree.appendChild(ul);
+
+    branchCount = 0;
+    subBranchCount = 0; 
+
+    buildTree(baseData, ul, checkboxFunction);
+
+    var div = document.getElementById(divId);
+    clearElement(div);
+    div.appendChild(tree);
+}
+
+function buildTree(baseData, baseElement, checkboxFunction) {
+    var dataLength = baseData.length;
+    for(var i = 0; i < dataLength; i++){
+        var base = baseData[i];
+        var baseName = getAttribute(base.Attributes, 'UserName');
+        var li = document.createElement('li');
+        var ul = createUL();
+
+        appendListItemChildren(li, 'Site'.concat(base.GUID), checkboxFunction, 'Site', baseName, ul, baseName, base.GUID);
+
+        baseElement.appendChild(li);        
+    }
+}
+
+function appendListItemChildren(li, id, checkboxFunction, checkboxBranch, branchOption, ul, linkedSite, guid) {
+    li.appendChild(createBranchDiv(id));
+    li.appendChild(createCheckbox(id, checkboxFunction, checkboxBranch, linkedSite, guid));
+    li.appendChild(createTreeIcon(branchOption));
+    li.appendChild(createSpan(id, branchOption));
+    li.appendChild(createBranchListDiv(id.concat('List'), ul));
+}
+
+function createBranchDiv(branchDivId) {
+    var branchDiv = document.createElement('div');
+    branchDiv.id = branchDivId;
+    branchDiv.setAttribute('style', 'padding-right: 4px;');
+    return branchDiv;
+}
+
+function createBranchListDiv(branchListDivId, ul) {
+    var branchListDiv = document.createElement('div');
+    branchListDiv.id = branchListDivId;
+    branchListDiv.setAttribute('class', 'listitem-hidden');
+    branchListDiv.appendChild(ul);
+    return branchListDiv;
+}
+
+function createUL() {
+    var ul = document.createElement('ul');
+    ul.setAttribute('class', 'format-listitem');
+    return ul;
+}
+
+function createTreeIcon(branch) {
+    var icon = document.createElement('i');
+    icon.setAttribute('class', getIconByBranch(branch));
+    icon.setAttribute('style', 'padding-left: 3px; padding-right: 3px;');
+    return icon;
+}
+
+function createSpan(spanId, innerHTML) {
+    var span = document.createElement('span');
+    span.id = spanId.concat('span');
+    span.innerHTML = innerHTML;
+    return span;
+}
+
+function createCheckbox(checkboxId, checkboxFunction, branch, linkedSite, guid) {
+    var functionArray = checkboxFunction.replace(')', '').split('(');
+    var functionArrayLength = functionArray.length;
+    var functionName = functionArray[0];
+    var functionArguments = [];
+
+    var checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';  
+    checkBox.id = checkboxId.concat('checkbox');
+    checkBox.setAttribute('Branch', branch);
+    checkBox.setAttribute('LinkedSite', linkedSite);
+    checkBox.setAttribute('GUID', guid);
+
+    functionArguments.push(checkBox.id);
+    if(functionArrayLength > 1) {
+        var functionArgumentLength = functionArray[1].split(',').length;
+        for(var i = 0; i < functionArgumentLength; i++) {
+            functionArguments.push(functionArray[1].split(',')[i]);
+        }
+    }
+    functionName = functionName.concat('(').concat(functionArguments.join(',').concat(')'));
+    
+    checkBox.setAttribute('onclick', functionName);
+    return checkBox;
+}
+
+function getIconByBranch(branch) {
+    return 'fas fa-user';
+}
+
+function addUser() {
+	var div = document.createElement('div');
+
+	var userNameTable = document.createElement('table');
+	userNameTable.id = 'userNameTable';
+	userNameTable.setAttribute('style', 'width: 100%;');
+
+	var addDetailsTable = document.createElement('table');
+	addDetailsTable.id = 'addDetailsTable';
+	addDetailsTable.setAttribute('style', 'width: 100%;');
+
+	var addRolesTable = document.createElement('table');
+	addRolesTable.id = 'addRolesTable';
+	addRolesTable.setAttribute('style', 'width: 100%;');
+
+	var userNameTableRow = document.createElement('tr');
+	userNameTableRow.appendChild(createTableHeader('border: solid black 1px;', 'Role'));
+	userNameTableRow.appendChild(createTableHeader('border: solid black 1px;', 'Value'));
+	userNameTableRow.appendChild(createTableHeader('border: solid black 1px;', ''));
+	userNameTable.appendChild(userNameTableRow);
+	userNameTable.appendChild(document.createElement('br'));
+
+	var detailsTableRow = document.createElement('tr');
+	detailsTableRow.appendChild(createTableHeader('border: solid black 1px;', 'Detail'));
+	detailsTableRow.appendChild(createTableHeader('border: solid black 1px;', 'Value'));
+	detailsTableRow.appendChild(createTableHeader('border: solid black 1px;', ''));
+	addDetailsTable.appendChild(detailsTableRow);
+	addDetailsTable.appendChild(document.createElement('br'));
+
+	var rolesTableRow = document.createElement('tr');
+	rolesTableRow.appendChild(createTableHeader('border: solid black 1px;', 'Role'));
+	rolesTableRow.appendChild(createTableHeader('border: solid black 1px;', 'Value'));
+	rolesTableRow.appendChild(createTableHeader('border: solid black 1px;', ''));
+	addRolesTable.appendChild(rolesTableRow);
+	addRolesTable.appendChild(document.createElement('br'));
+
+	var addUserNameTableRow = document.createElement('tr');
+	var addDetailsTableRow = document.createElement('tr');
+	var addRolessTableRow = document.createElement('tr');
+
+	for(var j = 0; j < 3; j++) {
+		var userNameTableDatacell = document.createElement('td');
+		var detailTableDatacell = document.createElement('td');
+		var roleTableDatacell = document.createElement('td');
+
+		userNameTableDatacell.setAttribute('style', 'border: solid black 1px;');
+		detailTableDatacell.setAttribute('style', 'border: solid black 1px;');
+		roleTableDatacell.setAttribute('style', 'border: solid black 1px;');
+		
+		if(j == 0) {
+			userNameTableDatacell.innerHTML = 'User Name';
+			detailTableDatacell.innerHTML = 'Select Detail Type';
+			roleTableDatacell.innerHTML = 'Select Role';
+		}
+		else if(j == 1) {
+			userNameTableDatacell.innerHTML = 'Enter User Name';
+			detailTableDatacell.innerHTML = 'Enter/Select Detail Value';
+			roleTableDatacell.innerHTML = 'Enter/Select Role Value';
+		}
+		else {
+			detailTableDatacell.innerHTML = 'Add/Delete Icon';
+			roleTableDatacell.innerHTML = 'Add/Delete Icon';
+		}
+
+		addUserNameTableRow.appendChild(userNameTableDatacell);
+		addDetailsTableRow.appendChild(detailTableDatacell);
+		addRolessTableRow.appendChild(roleTableDatacell);
+	}
+
+	userNameTable.appendChild(addUserNameTableRow);
+	addDetailsTable.appendChild(addDetailsTableRow);
+	addRolesTable.appendChild(addRolessTableRow);
+
+	div.appendChild(userNameTable);
+	div.appendChild(addDetailsTable);
+	div.appendChild(addRolesTable);
+
+	xdialog.confirm(div.outerHTML, function() {}, 
+	{
+		style: 'width:50%;font-size:0.8rem;',
+		buttons: {
+			ok: {
+				text: 'Save & Close',
+				style: 'background: Green;'
+			}
+		},
+		title: 'Add New User'
+	});
+}
+
+function deleteUsers() {
+	xdialog.confirm('List users to delete here', function() {
+	}, {
+		style: 'width:420px;font-size:0.8rem;',
+		title: 'Are You Sure You Want To Delete These Users?',
+		buttons: {
+			ok: {
+				text: 'Delete Users',
+				style: 'background: red;',
+			},
+			cancel: {
+				text: 'Cancel',
+				style: 'background: Green;',
+			}
+		}
+	});
+}
+
+function reinstateUsers() {
+	xdialog.confirm('List users that can be reinstated here', function() {
+	}, {
+		style: 'width:420px;font-size:0.8rem;',
+		title: 'Select Users To Reinstate',
+		buttons: {
+			ok: {
+				text: 'Reinstate Users',
+				style: 'background: green;',
+			},
+			cancel: {
+				text: 'Cancel',
+				style: 'background: grey;',
+			}
+		}
+	});
+}
+
+function getAttribute(attributes, attributeRequired) {
+	for (var attribute in attributes) {
+		var array = attributes[attribute];
+
+		for(var key in array) {
+			if(key == attributeRequired) {
+				return array[key];
+			}
+		}
+	}
+
+	return null;
+}
+
+function clearElement(element) {
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
+}
+
+function updateClassOnClick(elementId, firstClass, secondClass){
+	var elements = document.getElementsByClassName(elementId);
+
+	if(elements.length == 0) {
+		var element = document.getElementById(elementId);
+		updateClass(element, firstClass, secondClass);
+	}
+	else {
+		for(var i = 0; i< elements.length; i++) {
+			updateClass(elements[i], firstClass, secondClass)
+		}
+	}
+}
+
+function updateClass(element, firstClass, secondClass)
+{
+	if(hasClass(element, firstClass)){
+		element.classList.remove(firstClass);
+
+		if(secondClass != ''){
+			element.classList.add(secondClass);
+		}
+	}
+	else {
+		if(secondClass != ''){
+			element.classList.remove(secondClass);
+		}
+		
+		element.classList.add(firstClass);
+	}
+}
+  
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+function addExpanderOnClickEvents() {
+	var expanders = document.getElementsByClassName('fa-plus-square');
+	var expandersLength = expanders.length;
+	for(var i = 0; i < expandersLength; i++){
+		addExpanderOnClickEventsByElement(expanders[i]);
+	}
+}
+
+function addExpanderOnClickEventsByElement(element) {
+	element.addEventListener('click', function (event) {
+		updateClassOnClick(this.id, 'fa-plus-square', 'fa-minus-square')
+		updateClassOnClick(this.id.concat('List'), 'listitem-hidden', '')
+	});
+
+	updateAdditionalControls(element);
+	expandAdditionalLists(element);
+}
+
+function updateAdditionalControls(element) {
+	var additionalcontrols = element.getAttribute('additionalcontrols');
+
+	if(!additionalcontrols) {
+		return;
+	}
+
+	var listToHide = element.id.concat('List');
+	var clickEventFunction = function (event) {
+		updateClassOnClick(listToHide, 'listitem-hidden', '')
+	};
+
+	var controlArray = additionalcontrols.split(',');
+	for(var j = 0; j < controlArray.length; j++) {
+		var controlId = controlArray[j];	
+
+		element.addEventListener('click', function (event) {
+			var controlElement = document.getElementById(controlId);
+			if(hasClass(this, 'fa-minus-square')) {				
+				controlElement.addEventListener('click', clickEventFunction, false);
+			}
+			else {
+				controlElement.removeEventListener('click', clickEventFunction);
+			}
+		});
+	}	
+}
+
+function expandAdditionalLists(element) {
+	var additionalLists = element.getAttribute('additionallists');
+
+	if(!additionalLists) {
+		return;
+	}
+
+	element.addEventListener('click', function (event) {
+		var controlArray = additionalLists.split(',');
+		for(var j = 0; j < controlArray.length; j++) {
+			var controlId = controlArray[j];
+			var controlElement = document.getElementById(controlId);
+			updateClass(controlElement, 'listitem-hidden', '');
+		}
+	});		
+}
+
+function showHideIcon(iconId, style) {
+	var icon = document.getElementById(iconId);
+	icon.setAttribute('style', style);
+}
+
+function createIcon(iconId, className, style, onClickEvent, title) {
+	var icon = document.createElement('i');
+	icon.id = iconId;
+	icon.setAttribute('class', className);
+
+	if(style) {
+		icon.setAttribute('style', style);
+	}
+
+	if(onClickEvent) {
+		icon.setAttribute('onclick', onClickEvent);
+	}
+
+	if(title) {
+		icon.setAttribute('title', title);
+	}
+
+	return icon;
+}
+
+function createTableHeader(style, value) {
+	var tableHeader = document.createElement('th');
+
+	if(style != '') {
+		tableHeader.setAttribute('style', style);
+	}
+	
+	tableHeader.innerHTML = value;
+	return tableHeader;
 }
