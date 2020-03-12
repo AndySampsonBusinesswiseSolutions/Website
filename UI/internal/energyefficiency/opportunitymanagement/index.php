c43ef617ab5949fb471a28fc07b71790489ecb47
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-03-12 11:17:30 +0000

    0-portal-project
    Opportunity Management
    -> Add 'Add Opportunities' button to Recommended Opportunities and Active & Future Opportunities sections -> Ask for customer then load Create Opportunities page
    -> Change Active & Future Projects to Pending & Active Opportunities

--- UI/internal/energyefficiency/opportunitymanagement/index.php
+++ UI/internal/energyefficiency/opportunitymanagement/index.php
@@ -108,34 +108,33 @@
 							<thead>
 								<tr>
 									<th id="th0" style="border: solid black 1px;">Customer</th>
-									<th id="th1" style="border: solid black 1px;">Project<br>Name</th>
+									<th id="th1" style="border: solid black 1px;">Opportunity<br>Name</th>
 									<th id="th2" style="border: solid black 1px;">Site</th>
 									<th id="th3" style="border: solid black 1px;">Meter</th>
-									<th id="th10" style="border: solid black 1px;">Notes</th>
 									<th id="th4" style="border: solid black 1px;">Engineer</th>
 									<th id="th5" style="border: solid black 1px;">Estimated<br>Start Date</th>
 									<th id="th6" style="border: solid black 1px;">Estimated<br>Finish Date</th>
 									<th id="th7" style="border: solid black 1px;">Estimated<br>Cost</th>
 									<th id="th8" style="border: solid black 1px;">Estimated<br>kWh Savings (pa)</th>
 									<th id="th9" style="border: solid black 1px;">Estimated<br>£ Savings (pa)</th>
-									<th id="th11" style="border: solid black 1px;" colspan="2">Manage Project</th>
+									<th id="th11" style="border: solid black 1px;" colspan="3">Manage Opportunity</th>
 								</tr>
 							</thead>
 							<tbody class="scrollContent3">
 								<tr>
 									<td headers="th0" style="border: solid black 1px;">David Ford Trading Ltd</td>
-									<td headers="th1" style="border: solid black 1px;">LED Lighting</td>
+									<td headers="th1" style="border: solid black 1px;">LED Lighting <i class="fas fa-search show-pointer"></i></td>
 									<td headers="th2" style="border: solid black 1px;">Site X</td>
 									<td headers="th3" style="border: solid black 1px;">N/A</td>
-									<td headers="th10" style="border: solid black 1px;"><i class="fas fa-search show-pointer"></i></td>
 									<td headers="th4" style="border: solid black 1px;">En Gineer</td>
 									<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
 									<td headers="th6" style="border: solid black 1px;">30/06/2020</td>
 									<td headers="th7" style="border: solid black 1px;">£100,000</td>
 									<td headers="th8" style="border: solid black 1px;">10,000</td>
 									<td headers="th9" style="border: solid black 1px;">£15,000</td>
-									<td headers="th11" style="border: solid black 1px;"><button class="show-pointer">Approve Project</button></td>
-									<td headers="th12" style="border: solid black 1px;"><button class="show-pointer">Reject Project</button></td>
+									<td headers="th11" style="border: solid black 1px;"><button class="show-pointer">Approve Opportunity</button></td>
+									<td headers="th12" style="border: solid black 1px;"><button class="show-pointer">Reject Opportunity</button></td>
+									<td headers="th13" style="border: solid black 1px;"><button class="show-pointer" style="margin: 2px;">Add Opportunities</button></td>
 								</tr>
 							</tbody>
 						</table>
@@ -148,7 +147,7 @@
 				<div class="divcolumn first"></div>
 				<div class="roundborder divcolumn" style="width: 94%; background-color: #e9eaee;">
 					<div style="text-align: center; border-bottom: solid black 1px;">
-						<span>Active & Future Projects</span>
+						<span>Pending & Active Opportunities</span>
 					</div>
 					<br>
 					<div id="tableContainer" class="tableContainer4">
@@ -156,35 +155,34 @@
 							<thead>
 								<tr>
 									<th id="th0" style="border: solid black 1px;">Customer</th>
-									<th id="th1" style="border: solid black 1px;">Project<br>Name</th>
+									<th id="th1" style="border: solid black 1px;">Opportunity<br>Name</th>
 									<th id="th2" style="border: solid black 1px;">Site</th>
 									<th id="th3" style="border: solid black 1px;">Meter</th>
-									<th id="th10" style="border: solid black 1px;">Notes</th>
 									<th id="th4" style="border: solid black 1px;">Engineer</th>
 									<th id="th5" style="border: solid black 1px;">Estimated<br>Start Date</th>
 									<th id="th6" style="border: solid black 1px;">Estimated<br>Finish Date</th>
 									<th id="th7" style="border: solid black 1px;">Estimated<br>Cost</th>
 									<th id="th8" style="border: solid black 1px;">Estimated<br>kWh Savings (pa)</th>
 									<th id="th9" style="border: solid black 1px;">Estimated<br>£ Savings (pa)</th>
-									<th id="th11" style="border: solid black 1px;" colspan="3">Manage Project</th>
+									<th id="th11" style="border: solid black 1px;" colspan="4">Manage Opportunity</th>
 								</tr>
 							</thead>
 							<tbody class="scrollContent3">
 								<tr>
 									<td headers="th0" style="border: solid black 1px;">David Ford Trading Ltd</td>
-									<td headers="th1" style="border: solid black 1px;">LED Lighting</td>
+									<td headers="th1" style="border: solid black 1px;">LED Lighting <i class="fas fa-search show-pointer"></i></td>
 									<td headers="th2" style="border: solid black 1px;">Site X</td>
 									<td headers="th3" style="border: solid black 1px;">N/A</td>
-									<td headers="th10" style="border: solid black 1px;"><i class="fas fa-search show-pointer"></i></td>
 									<td headers="th4" style="border: solid black 1px;">En Gineer</td>
 									<td headers="th5" style="border: solid black 1px;">01/04/2020</td>
 									<td headers="th6" style="border: solid black 1px;">30/06/2020</td>
 									<td headers="th7" style="border: solid black 1px;">£100,000</td>
 									<td headers="th8" style="border: solid black 1px;">10,000</td>
 									<td headers="th9" style="border: solid black 1px;">£15,000</td>
-									<td headers="th11" style="border: solid black 1px;"><button class="show-pointer">Close Project</button></td>
-									<td headers="th12" style="border: solid black 1px;"><button class="show-pointer">Manage Project Details</button></td>
-									<td headers="th13" style="border: solid black 1px;"><button class="show-pointer">Manage Project Timelines</button></td>
+									<td headers="th11" style="border: solid black 1px;"><button class="show-pointer" style="margin: 2px;">Close Opportunity</button></td>
+									<td headers="th12" style="border: solid black 1px;"><button class="show-pointer" style="margin: 2px;">Manage Opportunity Details</button></td>
+									<td headers="th13" style="border: solid black 1px;"><button class="show-pointer" style="margin: 2px;">Manage Opportunity Timelines</button></td>
+									<td headers="th14" style="border: solid black 1px;"><button class="show-pointer" style="margin: 2px;">Add Opportunities</button></td>
 								</tr>
 							</tbody>
 						</table>
