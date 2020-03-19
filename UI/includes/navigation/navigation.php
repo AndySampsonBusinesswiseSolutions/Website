a93d2156962ebff5f9b84b0b88de9d411e3b3532
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-03-19 16:20:53 +0000

    0-portal-project
    Bring all BWS css into project and correct to make pages work

--- UI/includes/navigation/navigation.php
+++ UI/includes/navigation/navigation.php
@@ -4,10 +4,9 @@
     <div class="fusion-header-v1 fusion-mobile-logo-1  fusion-mobile-menu-design-modern">
         <div class="fusion-header">
             <div class="fusion-row">
-                <div class="fusion-logo" data-margin-top="20px" data-margin-bottom="0px" data-margin-left="0px" data-margin-right="0px">
-                    <a class="fusion-logo-link" href="https://www.businesswisesolutions.co.uk/">
-                        <!-- standard logo -->
-                        <img src="https://www.businesswisesolutions.co.uk/wp-content/uploads/2018/05/bws-logo-white.png" srcset="https://www.businesswisesolutions.co.uk/wp-content/uploads/2018/05/bws-logo-white.png 1x" alt="Businesswise Solutions Logo" retina_logo_url="" class="fusion-standard-logo">
+                <div class="fusion-logo">
+                    <a class="fusion-logo-link" href="https://www.businesswisesolutions.co.uk/" style="float: left;">
+                        <img src="https://www.businesswisesolutions.co.uk/wp-content/uploads/2017/10/bws-logo-web.png" srcset="https://www.businesswisesolutions.co.uk/wp-content/uploads/2017/10/bws-logo-web.png 1x, https://www.businesswisesolutions.co.uk/wp-content/uploads/2017/10/bws-logo-web.png 2x" width="291" height="78" style="max-height:78px;height:auto;" alt="Businesswise Solutions Logo" data-retina_logo_url="https://www.businesswisesolutions.co.uk/wp-content/uploads/2017/10/bws-logo-web.png" class="fusion-standard-logo">
                     </a>
                 </div>
                 <?php if ($PAGE_TITLE == "Login") { ?>
@@ -27,15 +26,15 @@
                 <?php if ($PAGE_TITLE != "Login" && $PAGE_TITLE != "Forgotten Password") { ?>
                     <nav class="fusion-main-menu" aria-label="Main Menu">
                         <ul id="menu-main-menu" class="fusion-menu">
-                            <li style="padding-right: 25px;"><a href="/Internal/Dashboard/" class="fusion-flex-link fusion-bar-highlight"><i class="fas fa-home" title="Dashboard"></i></a></li>
-                            <li style="padding-right: 25px;"><a href="#" class="fusion-flex-link fusion-bar-highlight" aria-haspopup="true"><i class="fas fa-coins"></i><span class="fusion-caret"></span><span>Finance</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
+                            <li style="padding-right: 25px;"><a href="/Internal/Dashboard/"><i class="fas fa-home" title="Dashboard"></i></a></li>
+                            <li style="padding-right: 25px;"><a href="#"><i class="fas fa-coins"></i><span class="fusion-caret"></span><span>Finance</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
                                 <ul class="sub-menu">
                                     <li class="submenu-item"><a href="/Internal/Finance/BillValidation/"><span>Bill Validation</span></a></li>
                                     <li class="submenu-item"><a href="/Internal/Finance/Commissions/"><span>Commissions</span></a></li>
                                     <li class="submenu-item"><a href="/Internal/Finance/VarianceAnalysis/"><span>Variance Analysis</span></a></li>
                                 </ul>
                             </li>
-                            <li style="padding-right: 25px;"><a href="#" class="fusion-flex-link fusion-bar-highlight" aria-haspopup="true"><i class="fas fa-cog"></i><span class="fusion-caret"></span><span>Energy Efficiency</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
+                            <li style="padding-right: 25px;"><a href="#"><i class="fas fa-cog"></i><span class="fusion-caret"></span><span>Energy Efficiency</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
                                 <ul class="sub-menu">
                                     <li class="submenu-item"><a href="/Internal/EnergyEfficiency/OpportunitiesDashboard/"><span>Opportunities Dashboard</span></a></li>
                                     <li class="submenu-item"><a href="/Internal/EnergyEfficiency/Pending&ActiveOpportunities/"><span>Pending & Active Opportunities</span></a></li>
@@ -43,7 +42,7 @@
                                     <li class="submenu-item"><a href="/Internal/EnergyEfficiency/OpportunityManagement/"><span>Opportunity Management</span></a></li>
                                 </ul>
                             </li>
-                            <li style="padding-right: 25px;"><a href="#" class="fusion-flex-link fusion-bar-highlight" aria-haspopup="true"><i class="fas fa-list-alt"></i><span class="fusion-caret"></span><span>Portfolio Management</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
+                            <li style="padding-right: 25px;"><a href="#"><i class="fas fa-list-alt"></i><span class="fusion-caret"></span><span>Portfolio Management</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
                                 <ul class="sub-menu">
                                     <li class="submenu-item"><a href="/Internal/PortfolioManagement/SiteManagement/"><span>Site Management</span></a></li>
                                     <li class="submenu-item"><a href="/Internal/PortfolioManagement/ContractManagement/"><span>Contract Management</span></a></li>
@@ -51,13 +50,13 @@
                                     <li class="submenu-item"><a href="/Internal/PortfolioManagement/ViewMeterConsumption/"><span>View Meter Consumption</span></a></li>
                                 </ul>
                             </li>
-                            <li style="padding-right: 25px;"><a href="#" class="fusion-flex-link fusion-bar-highlight" aria-haspopup="true"><i class="fas fa-lightbulb"></i><span class="fusion-caret"></span><span>Supplier Management</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
+                            <li style="padding-right: 25px;"><a href="#"><i class="fas fa-lightbulb"></i><span class="fusion-caret"></span><span>Supplier Management</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
                                 <ul class="sub-menu">
                                     <li class="submenu-item"><a href="/Internal/SupplierManagement/SupplierManagement/"><span>Supplier Management</span></a></li>
                                     <li class="submenu-item"><a href="/Internal/SupplierManagement/SupplierProductManagement/"><span>Supplier Product Management</span></a></li>
                                 </ul>
                             </li>
-                            <li style="padding-right: 25px;"><a href="#" class="fusion-flex-link fusion-bar-highlight" aria-haspopup="true"><i class="fas fa-user"></i><span class="fusion-caret"></span><span>Account Management</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
+                            <li style="padding-right: 25px;"><a href="#"><i class="fas fa-user"></i><span class="fusion-caret"></span><span>Account Management</span><span class="fusion-caret"><i class="fas fa-chevron-down"></i></span></a>
                                 <ul class="sub-menu">
                                     <li class="submenu-item"><a href="/Internal/AccountManagement/MyProfile/"><span>My Profile</span></a></li>
                                     <li class="submenu-item"><a href="/Internal/AccountManagement/ManageUsers/"><span>Manage Users</span></a></li>
@@ -65,7 +64,7 @@
                                     <li class="submenu-item"><a href="/Internal/AccountManagement/MyDocuments/"><span>My Documents</span></a></li>
                                 </ul>
                             </li>
-                            <li><a href="/" class="fusion-flex-link fusion-bar-highlight"><i class="fas fa-sign-out-alt" title="Logout"></i></a></li>
+                            <li><a href="/"><i class="fas fa-sign-out-alt" title="Logout"></i></a></li>
                         </ul>
                     </nav>
                 <?php } ?>
