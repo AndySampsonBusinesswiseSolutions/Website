993b0d456ac13fd52d5fe268dd8e1377dc482f2e
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-04-29 16:11:32 +0100

    0-portal-project
    All Pages
    -> Update Pending & Active Opportunities to use base.js

--- UI/internal/energyefficiency/pending&activeopportunities/index.php
+++ UI/internal/energyefficiency/pending&activeopportunities/index.php
@@ -13,11 +13,12 @@
 
 <body>
 	<div id="mySidenav" class="sidenav">
+		<i class="fas fa-unlock fa-w-14 sidenav-icon lock" onclick="lockSidebar()" title="Click To Lock Sidebar"></i>
 		<i class="fas fa-angle-double-left sidenav-icon closebtn" onclick="closeNav()"></i>
 		<div class="tree-column">
 			<div class="tree-div roundborder">
 				<span style="padding-left: 5px;">Select Tree Display Order</span>
-				<div id="treeDisplayOrder" class="far fa-plus-square show-pointer"></div>
+				<div id="treeDisplayOrder" class="far fa-plus-square show-pointer expander openExpander"></div>
 				<ul id="treeDisplayOrderList" class="format-listitem">
 					<li>
 						<input type="radio" name="group1" value="Project" id="projectOrderradio" checked onclick='createTree(activeopportunity, "treeDiv", "updateGanttChartAndDataGrid()");'><span id="projectOrderspan" style="padding-left: 1px;">Project -> Site -> Meter</span>
@@ -33,7 +34,7 @@
 			<br>
 			<div class="tree-div roundborder">
 				<span style="padding-left: 5px;">Select Project Status</span>
-				<div id="projectStatus" class="far fa-plus-square show-pointer"></div>
+				<div id="projectStatus" class="far fa-plus-square show-pointer expander openExpander"></div>
 					<ul id="projectStatusList" class="format-listitem">
 						<li>
 							<input type="radio" name="group2" value="All" id="allStatusradio" checked onclick='updateGanttChartAndDataGrid()'><span id="allStatusspan" style="padding-left: 1px;">All</span>
@@ -63,7 +64,7 @@
 		<div class="roundborder tree-div scrolling-wrapper">
 			<div style="text-align: center; border-bottom: solid black 1px;">
 				<span>Pending & Active Opportunities Chart</span>
-				<i class="far fa-plus-square show-pointer" id="pendingAndActiveOpportunities"></i>
+				<i class="far fa-plus-square show-pointer expander openExpander" id="pendingAndActiveOpportunities"></i>
 				<div class="fas fa-cart-arrow-down show-pointer" style="float: right;" title="Add Pending & Active Opportunities To Download Basket"></div>
 				<div class="fas fa-download show-pointer" style="margin-right: 5px; float: right;" title="Download Pending & Active Opportunities"></div>
 			</div>
@@ -74,6 +75,8 @@
 	</div>
 	<br>
 </body>
+
+<script src="/includes/base.js"></script>
 
 <script type="text/javascript" src="jquery-1.4.2.js"></script>
 <script type="text/javascript" src="https://bossanova.uk/jexcel/v3/jexcel.js"></script>
