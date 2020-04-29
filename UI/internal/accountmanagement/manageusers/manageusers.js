32c3db404065bfe37b2d66411e4b3741bd7b5853
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-04-29 17:23:08 +0100

    0-portal-project
    All Pages
    -> Update Manage Users to use base.js

--- UI/internal/accountmanagement/manageusers/manageusers.js
+++ UI/internal/accountmanagement/manageusers/manageusers.js
@@ -1,29 +1,14 @@
 function pageLoad() {    
 	createTree(user, "treeDiv", "createCardButton");
 	
-	document.onmousemove=function(e) {
-		var mousecoords = getMousePos(e);
-		if(mousecoords.x <= 25) {
-			openNav();
-		}  
-		else if(mousecoords.x >= 400) {
-			closeNav();
-		}  
+	document.onmousemove = function(e) {
+		setupSidebarHeight();
+		setupSidebar(e);
 	};
-}
 
-function getMousePos(e) {
-	return {x:e.clientX,y:e.clientY};
-}
-
-function openNav() {
-	document.getElementById("mySidenav").style.width = "400px";
-	document.getElementById("openNav").style.color = "#b62a51";
-}
-
-function closeNav() {
-	document.getElementById("openNav").style.color = "white";
-	document.getElementById("mySidenav").style.width = "0px";
+	window.onscroll = function() {
+		setupSidebarHeight();
+	};
 }
 
 function openTab(callingElement, tabName, guid) {
@@ -350,16 +335,6 @@ function deleteRow(row) {
     finalisePopup(title, 'Delete Attribute?<br><br>', modal, span);
 }
 
-function finalisePopup(title, titleHTML, modal, span) {
-    title.innerHTML = titleHTML;
-
-	modal.style.display = "block";
-
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-}
-
 function saveRow(row) {
 	var textBox = document.getElementById('input' + row);
 	var tableDatacell = document.getElementById('value' + row);
@@ -473,7 +448,7 @@ function createTree(baseData, divId, checkboxFunction) {
 
     var header = document.createElement('span');
     header.style = "padding-left: 5px;";
-    header.innerHTML = 'Select User(s) <i class="far fa-plus-square show-pointer"" id="' + divId.concat('Selector') + '"></i>';
+    header.innerHTML = 'Select User(s) <i class="far fa-plus-square show-pointer expander openExpander" id="' + divId.concat('Selector') + '"></i>';
 
     div.appendChild(header);
 	div.appendChild(tree);
@@ -483,6 +458,7 @@ function createTree(baseData, divId, checkboxFunction) {
 	openTab(document.getElementById('User2button'), 'User2button', '2');
 
 	addExpanderOnClickEvents();
+	setOpenExpanders();
 }
 
 function buildTree(baseData, baseElement, checkboxFunction) {
@@ -572,26 +548,6 @@ function getIconByBranch() {
 	return 'fas fa-user';
 }
 
-function clearElement(element) {
-	while (element.firstChild) {
-		element.removeChild(element.firstChild);
-	}
-}
-
-function getAttribute(attributes, attributeRequired) {
-	for (var attribute in attributes) {
-		var array = attributes[attribute];
-
-		for(var key in array) {
-			if(key == attributeRequired) {
-				return array[key];
-			}
-		}
-	}
-
-	return null;
-}
-
 function showHideIcon(iconId, style) {
 	var icon = document.getElementById(iconId);
 	icon.setAttribute('style', style);
@@ -626,59 +582,6 @@ function createTableHeader(style, value) {
 	
 	tableHeader.innerHTML = value;
 	return tableHeader;
-}
-
-function updateClassOnClick(elementId, firstClass, secondClass){
-	var elements = document.getElementsByClassName(elementId);
-
-	if(elements.length == 0) {
-		var element = document.getElementById(elementId);
-		updateClass(element, firstClass, secondClass);
-	}
-	else {
-		for(var i = 0; i< elements.length; i++) {
-			updateClass(elements[i], firstClass, secondClass)
-		}
-	}
-}
-
-function updateClass(element, firstClass, secondClass)
-{
-	if(hasClass(element, firstClass)){
-		element.classList.remove(firstClass);
-
-		if(secondClass != ''){
-			element.classList.add(secondClass);
-		}
-	}
-	else {
-		if(secondClass != ''){
-			element.classList.remove(secondClass);
-		}
-		
-		element.classList.add(firstClass);
-	}
-}
-  
-function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-
-function addExpanderOnClickEvents() {
-	var expanders = document.getElementsByClassName('fa-plus-square');
-	var expandersLength = expanders.length;
-	for(var i = 0; i < expandersLength; i++){
-		addExpanderOnClickEventsByElement(expanders[i]);
-	}
-
-	updateClassOnClick('treeDivSelector', 'fa-plus-square', 'fa-minus-square')
-}
-
-function addExpanderOnClickEventsByElement(element) {
-	element.addEventListener('click', function (event) {
-		updateClassOnClick(this.id, 'fa-plus-square', 'fa-minus-square')
-		updateClassOnClick(this.id.concat('List'), 'listitem-hidden', '')
-	});
 }
 
 function getEntityByGUID(guid) {
