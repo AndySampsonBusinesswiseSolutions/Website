3fb5627b4cf8177f63a50141769e500e0a8aeadd
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-05-26 08:56:04 +0100

    0-portal-project
    Database scripts to begin APIs

---
+++ Database/0050 - Create Table - [System].[ProcessDetail].sql
@@ -0,0 +1,93 @@
+USE [EMaaS]
+GO
+
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+IF  EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[System].[ProcessDetail]') AND type in (N'U'))
+DROP TABLE [System].[ProcessDetail]
+GO
+CREATE TABLE [System].[ProcessDetail]
+	(
+	ProcessDetailId bigint IDENTITY(1,1) NOT NULL,
+	EffectiveFromDateTime datetime NOT NULL,
+	EffectiveToDateTime datetime NOT NULL,
+	CreatedDateTime datetime NOT NULL,
+	CreatedByUserId bigint NOT NULL,
+	ProcessId bigint NOT NULL,
+	ProcessAttributeId bigint NOT NULL,
+	ProcessDetailDescription varchar(200) NOT NULL
+	)  ON [System]
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	PK_ProcessDetail PRIMARY KEY CLUSTERED 
+	(
+	ProcessDetailId
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [System]
+
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	DF_ProcessDetail_EffectiveFromDateTime DEFAULT GETDATE() FOR EffectiveFromDateTime
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	DF_ProcessDetail_EffectiveToDateTime DEFAULT '9999-12-31' FOR EffectiveToDateTime
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	DF_ProcessDetail_CreatedDateTime DEFAULT GETDATE() FOR CreatedDateTime
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	FK_ProcessDetail_CreatedByUserId FOREIGN KEY
+	(
+	CreatedByUserId
+	) REFERENCES [Administration.User].[User]
+	(
+	UserId
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+DECLARE @v sql_variant 
+SET @v = N'Foreign Key constraint joining [System].[ProcessDetail].CreatedByUserId to [Administration.User].[User].UserId'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'System', N'TABLE', N'ProcessDetail', N'CONSTRAINT', N'FK_ProcessDetail_CreatedByUserId'
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	FK_ProcessDetail_ProcessId FOREIGN KEY
+	(
+	ProcessId
+	) REFERENCES [System].[Process]
+	(
+	ProcessId
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+DECLARE @v sql_variant 
+SET @v = N'Foreign Key constraint joining [System].[ProcessDetail].ProcessId to [System].[Process].ProcessId'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'System', N'TABLE', N'ProcessDetail', N'CONSTRAINT', N'FK_ProcessDetail_ProcessId'
+GO
+ALTER TABLE [System].[ProcessDetail] ADD CONSTRAINT
+	FK_ProcessDetail_ProcessAttributeId FOREIGN KEY
+	(
+	ProcessAttributeId
+	) REFERENCES [System].[ProcessAttribute]
+	(
+	ProcessAttributeId
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+DECLARE @v sql_variant 
+SET @v = N'Foreign Key constraint joining [System].[ProcessDetail].ProcessAttributeId to [System].[ProcessAttribute].ProcessAttributeId'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'System', N'TABLE', N'ProcessDetail', N'CONSTRAINT', N'FK_ProcessDetail_ProcessAttributeId'
+GO
+ALTER TABLE [System].[ProcessDetail] SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
---
+++ Database/0057 - Create Table - [System].[ProcessArchiveAttribute].sql
@@ -0,0 +1,61 @@
+USE [EMaaS]
+GO
+
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+IF  EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[System].[ProcessArchiveAttribute]') AND type in (N'U'))
+DROP TABLE [System].[ProcessArchiveAttribute]
+GO
+CREATE TABLE [System].[ProcessArchiveAttribute]
+	(
+	ProcessArchiveAttributeId bigint IDENTITY(1,1) NOT NULL,
+	EffectiveFromDateTime datetime NOT NULL,
+	EffectiveToDateTime datetime NOT NULL,
+	CreatedDateTime datetime NOT NULL,
+	CreatedByUserId bigint NOT NULL,
+	ProcessArchiveAttributeDescription varchar(200) NOT NULL
+	)  ON [System]
+GO
+ALTER TABLE [System].[ProcessArchiveAttribute] ADD CONSTRAINT
+	PK_ProcessArchiveAttribute PRIMARY KEY CLUSTERED 
+	(
+	ProcessArchiveAttributeId
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [System]
+
+GO
+ALTER TABLE [System].[ProcessArchiveAttribute] ADD CONSTRAINT
+	DF_ProcessArchiveAttribute_EffectiveFromDateTime DEFAULT GETDATE() FOR EffectiveFromDateTime
+GO
+ALTER TABLE [System].[ProcessArchiveAttribute] ADD CONSTRAINT
+	DF_ProcessArchiveAttribute_EffectiveToDateTime DEFAULT '9999-12-31' FOR EffectiveToDateTime
+GO
+ALTER TABLE [System].[ProcessArchiveAttribute] ADD CONSTRAINT
+	DF_ProcessArchiveAttribute_CreatedDateTime DEFAULT GETDATE() FOR CreatedDateTime
+GO
+ALTER TABLE [System].[ProcessArchiveAttribute] ADD CONSTRAINT
+	FK_ProcessArchiveAttribute_CreatedByUserId FOREIGN KEY
+	(
+	CreatedByUserId
+	) REFERENCES [Administration.User].[User]
+	(
+	UserId
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+DECLARE @v sql_variant 
+SET @v = N'Foreign Key constraint joining [System].[ProcessArchiveAttribute].CreatedByUserId to [Administration.User].[User].UserId'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'System', N'TABLE', N'ProcessArchiveAttribute', N'CONSTRAINT', N'FK_ProcessArchiveAttribute_CreatedByUserId'
+GO
+ALTER TABLE [System].[ProcessArchiveAttribute] SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
