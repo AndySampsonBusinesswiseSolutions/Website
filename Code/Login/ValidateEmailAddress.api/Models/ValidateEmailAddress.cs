b3609bc11ea64f937ede20c4234301af004454ff
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-06-03 09:47:43 +0100

    0-portal-project
    Delete ValidateEmailAddress API

--- Code/Login/ValidateEmailAddress.api/Models/ValidateEmailAddress.cs
+++
@@ -1,9 +0,0 @@
-namespace ValidateEmailAddress.api
-{
-    public class ValidateEmailAddress
-    {
-        public string Page {get;set;}
-        public string Process {get;set;}
-        public object Data {get;set;}
-    }
-}
