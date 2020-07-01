2f16f46fc113e775d40c41e06624dad836bf9b85
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-07-01 12:10:41 +0100

    0-portal-project
    Scripts for:
    -> SaveUsageUpload API
    -> Site Management Page
    -> UsageUpload Process

---
+++ Database/999 - Data/0044 - Data - Site Management Page Name Page Detail.sql
@@ -0,0 +1,15 @@
+USE [EMaaS]
+GO
+
+DECLARE @CreatedByUserId BIGINT = (SELECT UserId FROM [Administration.User].[User] WHERE UserGUID = '743E21EE-2185-45D4-9003-E35060B751E2')
+DECLARE @SourceAttributeId BIGINT = (SELECT SourceAttributeId FROM [Information].[SourceAttribute] WHERE SourceAttributeDescription = 'User Generated')
+DECLARE @SourceId BIGINT = (SELECT SourceId FROM [Information].[SourceDetail] WHERE SourceAttributeId = @SourceAttributeId AND SourceDetailDescription = @CreatedByUserId)
+DECLARE @PageId BIGINT = (SELECT PageId FROM [System].[Page] WHERE PageGUID = '714F10C4-ACF3-4409-97A8-C605E8E2FD0C')
+DECLARE @PageAttributeId BIGINT = (SELECT PageAttributeId FROM [System].[PageAttribute] WHERE PageAttributeDescription = 'Page Name')
+
+EXEC [System].[PageDetail_Insert] 
+    @CreatedByUserId, 
+    @SourceId,
+    @PageId,
+    @PageAttributeId,
+    'Site Management'
---
+++ Database/999 - Data/0046 - Data - Usage Upload Process Name Process Detail.sql
@@ -0,0 +1,15 @@
+USE [EMaaS]
+GO
+
+DECLARE @CreatedByUserId BIGINT = (SELECT UserId FROM [Administration.User].[User] WHERE UserGUID = '743E21EE-2185-45D4-9003-E35060B751E2')
+DECLARE @SourceAttributeId BIGINT = (SELECT SourceAttributeId FROM [Information].[SourceAttribute] WHERE SourceAttributeDescription = 'User Generated')
+DECLARE @SourceId BIGINT = (SELECT SourceId FROM [Information].[SourceDetail] WHERE SourceAttributeId = @SourceAttributeId AND SourceDetailDescription = @CreatedByUserId)
+DECLARE @ProcessId BIGINT = (SELECT ProcessId FROM [System].[Process] WHERE ProcessGUID = '1ACFB189-9C95-4DCD-A21A-93CDB2928620')
+DECLARE @ProcessAttributeId BIGINT = (SELECT ProcessAttributeId FROM [System].[ProcessAttribute] WHERE ProcessAttributeDescription = 'Process Name')
+
+EXEC [System].[ProcessDetail_Insert] 
+    @CreatedByUserId, 
+    @SourceId,
+    @ProcessId,
+    @ProcessAttributeId,
+    'Upload Usage'
