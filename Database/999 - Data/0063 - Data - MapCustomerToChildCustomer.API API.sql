859cd066e9805565c234bc2ba5cbed4e11ac60ab
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-07-07 07:44:17 +0100

    0-portal-project
    Update API GUID in MapCustomerToChildCustomer data

--- Database/999 - Data/0063 - Data - MapCustomerToChildCustomer.API API.sql
+++ Database/999 - Data/0063 - Data - MapCustomerToChildCustomer.API API.sql
@@ -1,7 +1,7 @@
 USE [EMaaS]
 GO
 
-DECLARE @APIGUID UNIQUEIDENTIFIER = 'C151AD1B-C3AB-448F-8C8D-572328A8D1C7'
+DECLARE @APIGUID UNIQUEIDENTIFIER = '1EDBA521-11E9-4564-94E6-8D84C4192844'
 DECLARE @CreatedByUserId BIGINT = (SELECT UserId FROM [Administration.User].[User] WHERE UserGUID = '743E21EE-2185-45D4-9003-E35060B751E2')
 DECLARE @SourceAttributeId BIGINT = (SELECT SourceAttributeId FROM [Information].[SourceAttribute] WHERE SourceAttributeDescription = 'User Generated')
 DECLARE @SourceId BIGINT = (SELECT SourceId FROM [Information].[SourceDetail] WHERE SourceAttributeId = @SourceAttributeId AND SourceDetailDescription = @CreatedByUserId)
