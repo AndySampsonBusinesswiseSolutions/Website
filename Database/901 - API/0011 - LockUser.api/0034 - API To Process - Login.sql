938cb85f4b184b7422a89e860bc7c65a7eb676a9
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-07-13 16:18:24 +0100

    0-portal-project
    Script updates to make sure Login and Add New Customer processes work

--- Database/901 - API/0011 - LockUser.api/0034 - API To Process - Login.sql
+++ Database/901 - API/0011 - LockUser.api/0034 - API To Process - Login.sql
@@ -4,7 +4,7 @@ GO
 DECLARE @CreatedByUserId BIGINT = (SELECT UserId FROM [Administration.User].[User] WHERE UserGUID = '743E21EE-2185-45D4-9003-E35060B751E2')
 DECLARE @SourceAttributeId BIGINT = (SELECT SourceAttributeId FROM [Information].[SourceAttribute] WHERE SourceAttributeDescription = 'User Generated')
 DECLARE @SourceId BIGINT = (SELECT SourceId FROM [Information].[SourceDetail] WHERE SourceAttributeId = @SourceAttributeId AND SourceDetailDescription = @CreatedByUserId)
-DECLARE @APIId BIGINT = (SELECT APIId FROM [System].[API] WHERE APIGUID = '3BBFC2B6-2572-43CD-921A-A237000AC248')
+DECLARE @APIId BIGINT = (SELECT APIId FROM [System].[API] WHERE APIGUID = '0C1BAFAA-586D-48BB-8D0B-B0B56BE0CCD2')
 DECLARE @ProcessId BIGINT = (SELECT ProcessId FROM [System].[Process] WHERE ProcessGUID ='AF10359F-FD78-4345-9F26-EF5A921E72FD')
 
 EXEC [Mapping].[APIToProcess_Insert] @CreatedByUserId, @SourceId, @APIId, @ProcessId
