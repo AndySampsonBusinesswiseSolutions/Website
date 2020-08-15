2d6d0140185604dbdbeb0fa120763312e78e1dfb
Andy Sampson <andy.sampson@businesswisesolutions.co.uk>
2020-08-15 19:22:56 +0100

    CommitLineLossFactorClassToMeterData API
    Initial setup but requires LLFC proces to be built first

--- Code/CustomerDataUpload/CommitLineLossFactorClassToMeterData.api/Controllers/CommitLineLossFactorClassToMeterDataController.cs
+++ Code/CustomerDataUpload/CommitLineLossFactorClassToMeterData.api/Controllers/CommitLineLossFactorClassToMeterDataController.cs
@@ -6,6 +6,7 @@ using enums;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Linq;
+using System.Data;
 
 namespace CommitLineLossFactorClassToMeterData.api.Controllers
 {
@@ -18,9 +19,15 @@ namespace CommitLineLossFactorClassToMeterData.api.Controllers
         private readonly Methods.System _systemMethods = new Methods.System();
         private readonly Methods.Administration _administrationMethods = new Methods.Administration();
         private readonly Methods.Information _informationMethods = new Methods.Information();
+        private readonly Methods.Customer _customerMethods = new Methods.Customer();
+        private readonly Methods.Mapping _mappingMethods = new Methods.Mapping();
+        private readonly Methods.Temp.Customer _tempCustomerMethods = new Methods.Temp.Customer();
         private static readonly Enums.System.API.Name _systemAPINameEnums = new Enums.System.API.Name();
         private static readonly Enums.System.API.Password _systemAPIPasswordEnums = new Enums.System.API.Password();
         private static readonly Enums.System.API.GUID _systemAPIGUIDEnums = new Enums.System.API.GUID();
+        private readonly Enums.Customer.Meter.Attribute _customerMeterAttributeEnums = new Enums.Customer.Meter.Attribute();
+        // private readonly Enums.Information.LineLossFactorClass.Attribute _informationLineLossFactorClassAttributeEnums = new Enums.Information.LineLossFactorClass.Attribute();
+        private readonly Enums.Customer.DataUploadValidation.Entity _customerDataUploadValidationEntityEnums = new Enums.Customer.DataUploadValidation.Entity();
         private readonly Int64 commitLineLossFactorClassToMeterDataAPIId;
 
         public CommitLineLossFactorClassToMeterDataController(ILogger<CommitLineLossFactorClassToMeterDataController> logger)
@@ -66,10 +73,48 @@ namespace CommitLineLossFactorClassToMeterData.api.Controllers
                     return;
                 }
 
-                //TODO: API Logic
+                //TODO: Build once LLF process is built
 
-                //Get data from [Temp.CustomerDataUpload].[Meter] where CanCommit = 1
-                //Insert into [Mapping].[LineLossFactorClassToMeter]
+                // //Get data from [Temp.CustomerDataUpload].[Meter] where CanCommit = 1
+                // var meterDataRows = _tempCustomerMethods.Meter_GetByProcessQueueGUID(processQueueGUID);
+                // var commitableDataRows = _tempCustomerMethods.GetCommitableRows(meterDataRows);
+
+                // if(!commitableDataRows.Any())
+                // {
+                //     //Nothing to commit so update Process Queue and exit
+                //     _systemMethods.ProcessQueue_Update(processQueueGUID, commitLineLossFactorClassToMeterDataAPIId, false, null);
+                //     return;
+                // }
+
+                // var meterIdentifierMeterAttributeId = _customerMethods.MeterAttribute_GetMeterAttributeIdByMeterAttributeDescription(_customerMeterAttributeEnums.MeterIdentifier);
+                // var lineLossFactorClassGroupIdLineLossFactorClassAttributeId = _informationMethods.LineLossFactorClassAttribute_GetLineLossFactorClassAttributeIdByLineLossFactorClassAttributeDescription(_informationLineLossFactorClassAttributeEnums.LineLossFactorClassGroupId);
+
+                // foreach(var dataRow in commitableDataRows)
+                // {
+                //     //Get MeterId from [Customer].[MeterDetail] by MPXN
+                //     var mpxn = dataRow.Field<string>(_customerDataUploadValidationEntityEnums.MPXN);
+                //     var meterId = _customerMethods.MeterDetail_GetMeterDetailIdByMeterAttributeIdAndMeterDetailDescription(meterIdentifierMeterAttributeId, mpxn);
+
+                //     //Get LineLossFactorClassId from [Information].[LineLossFactorClassDetail]
+                //     var lineLossFactorClass = dataRow.Field<string>(_customerDataUploadValidationEntityEnums.LineLossFactorClass);
+                //     var lineLossFactorClassId = _informationMethods.LineLossFactorClassDetail_GetLineLossFactorClassIdByLineLossFactorClassAttributeIdAndLineLossFactorClassDetailDescription(lineLossFactorClassGroupIdLineLossFactorClassAttributeId, lineLossFactorClass);
+
+                //     if(lineLossFactorClassId == 0)
+                //     {
+                //         //Create new LineLossFactorClassGUID
+                //         var lineLossFactorClassGUID = Guid.NewGuid().ToString();
+
+                //         //Insert into [Customer].[LineLossFactorClass]
+                //         _informationMethods.LineLossFactorClass_Insert(createdByUserId, sourceId, lineLossFactorClassGUID);
+                //         lineLossFactorClassId = _informationMethods.LineLossFactorClass_GetLineLossFactorClassIdByLineLossFactorClassGUID(lineLossFactorClassGUID);
+
+                //         //Insert into [Customer].[LineLossFactorClassDetail]
+                //         _informationMethods.LineLossFactorClassDetail_Insert(createdByUserId, sourceId, lineLossFactorClassId, lineLossFactorClassGroupIdLineLossFactorClassAttributeId, lineLossFactorClass);
+                //     }
+
+                //     //Insert into [Mapping].[LineLossFactorClassToMeter]
+                //     _mappingMethods.LineLossFactorClassToMeter_Insert(createdByUserId, sourceId, lineLossFactorClassId, meterId);
+                // }
 
                 //Update Process Queue
                 _systemMethods.ProcessQueue_Update(processQueueGUID, commitLineLossFactorClassToMeterDataAPIId, false, null);
@@ -84,4 +129,3 @@ namespace CommitLineLossFactorClassToMeterData.api.Controllers
         }
     }
 }
-
