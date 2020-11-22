a2a01c5299007d4375e6f5a3809606d1457393c5
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:06:28 +0000

    Update APIs to Apps
    Create CommitMeterToProfileClassDataApp

---
+++ Code/CustomerDataUpload/CommitMeterToProfileClassDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitMeterToProfileClassDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
