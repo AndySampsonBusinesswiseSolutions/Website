7c93076e45db1ccc7b4a14611b9e4ccb4ed7a8fa
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:01:25 +0000

    Update APIs to Apps
    Create CommitCustomerToSiteDataApp

---
+++ Code/CustomerDataUpload/CommitCustomerToSiteDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitCustomerToSiteDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
