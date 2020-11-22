52f9231f8bfd8a261c2a3f6a0ef3334557f6caaa
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:09:15 +0000

    Update APIs to Apps
    Create CommitSubMeterUsageDataApp

---
+++ Code/CustomerDataUpload/CommitSubMeterUsageDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitSubMeterUsageDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
