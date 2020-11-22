9733ccf965ba9ab29d5357baffff5dcd4c85201f
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:01:57 +0000

    Update APIs to Apps
    Create CommitEstimatedAnnualUsageApp

---
+++ Code/CustomerDataUpload/CommitEstimatedAnnualUsageApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitEstimatedAnnualUsageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
