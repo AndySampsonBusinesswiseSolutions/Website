2508c489a666714778dc57ebe450b8d44259c6a9
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 17:59:32 +0000

    Update APIs to Apps
    Create CommitContractDataApp

---
+++ Code/CustomerDataUpload/CommitContractDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitContractDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
