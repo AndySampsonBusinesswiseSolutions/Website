86ec2e96b348fad25d0d6f0fbf60c9602f8155df
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:18:25 +0000

    Update APIs to Apps
    Create ValidateSubMeterUsageDataApp

---
+++ Code/CustomerDataUpload/ValidateSubMeterUsageDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidateSubMeterUsageDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
