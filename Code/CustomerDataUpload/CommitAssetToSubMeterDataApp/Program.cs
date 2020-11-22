ec8ce141ed1af83b2e3fdc5a4179795d1bda46d6
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 17:58:12 +0000

    Update APIs to Apps
    Create CommitAssetToSubMeterDataApp

---
+++ Code/CustomerDataUpload/CommitAssetToSubMeterDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitAssetToSubMeterDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
