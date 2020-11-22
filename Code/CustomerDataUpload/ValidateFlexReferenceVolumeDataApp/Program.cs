4d7e62079d25bdb79b772947b739cbd6ee08c375
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:15:33 +0000

    Update APIs to Apps
    Create ValidateFlexReferenceVolumeDataApp

---
+++ Code/CustomerDataUpload/ValidateFlexReferenceVolumeDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidateFlexReferenceVolumeDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
