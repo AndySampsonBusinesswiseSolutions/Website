fd8818b79444d6b068b84ad696a93784e0902cd3
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:05:01 +0000

    Update APIs to Apps
    Create CommitLocalDistributionZoneToMeterDataApp

---
+++ Code/CustomerDataUpload/CommitLocalDistributionZoneToMeterDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitLocalDistributionZoneToMeterDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
