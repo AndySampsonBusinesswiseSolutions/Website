ac3acf8a659a5548344d674dec4a7160223e17bf
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 17:58:50 +0000

    Update APIs to Apps
    Create CommitBasketDataApp

---
+++ Code/CustomerDataUpload/CommitBasketDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitBasketDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
