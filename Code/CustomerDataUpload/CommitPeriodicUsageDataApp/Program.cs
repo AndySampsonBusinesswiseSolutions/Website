b3526513f22ee21966630dd15e757f46c63e7c2a
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:07:47 +0000

    Update APIs to Apps
    Create CommitPeriodicUsageDataApp

---
+++ Code/CustomerDataUpload/CommitPeriodicUsageDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitPeriodicUsageDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
