5fa5492a5f0f9a1466c4916d520047456eecb357
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:02:21 +0000

    Update APIs to Apps
    Create CommitFixedContractDataApp

---
+++ Code/CustomerDataUpload/CommitFixedContractDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitFixedContractDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
