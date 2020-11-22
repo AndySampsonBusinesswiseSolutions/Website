122bb37d632c4b8e73d2e3819c72af49d92fdfbf
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:14:52 +0000

    Update APIs to Apps
    Create ValidateFixedContractDataApp

---
+++ Code/CustomerDataUpload/ValidateFixedContractDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidateFixedContractDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
