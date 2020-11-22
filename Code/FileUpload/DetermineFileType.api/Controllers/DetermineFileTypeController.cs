1f7f8906a9216a85564350dff64bc74a13e9c0f1
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 16:11:53 +0000

    Update APIs to Apps
    Create Apps for each FileUpload API
    Update APIs to launch Apps

--- Code/FileUpload/DetermineFileType.api/Controllers/DetermineFileTypeController.cs
+++ Code/FileUpload/DetermineFileType.api/Controllers/DetermineFileTypeController.cs
@@ -44,104 +44,14 @@ namespace DetermineFileType.api.Controllers
         [Route("DetermineFileType/Determine")]
         public void Determine([FromBody] object data)
         {
-            var systemAPIGUIDEnums = new Enums.SystemSchema.API.GUID();
-            var informationMethods = new Methods.InformationSchema();
-            var systemAPIMethods = new Methods.SystemSchema.API();
-            var systemMethods = new Methods.SystemSchema();
-
-            //Get base variables
-            var createdByUserId = new Methods.AdministrationSchema.User().GetSystemUserId();
-            var sourceId = informationMethods.GetSystemUserGeneratedSourceId();
-
-            //Get Queue GUID
-            var jsonObject = JObject.Parse(data.ToString());
-            var processQueueGUID = systemMethods.GetProcessQueueGUIDFromJObject(jsonObject);
-
-            try
-            {
-                //Insert into ProcessQueue
-                systemMethods.ProcessQueue_Insert(
-                    processQueueGUID, 
-                    createdByUserId,
-                    sourceId,
-                    determineFileTypeAPIId);
-
-                if(!systemAPIMethods.PrerequisiteAPIsAreSuccessful(systemAPIGUIDEnums.DetermineFileTypeAPI, determineFileTypeAPIId, hostEnvironment, jsonObject))
-                {
-                    return;
-                }
-
-                //Update Process Queue
-                systemMethods.ProcessQueue_UpdateEffectiveFromDateTime(processQueueGUID, determineFileTypeAPIId);
-
-                //Get FileId by FileGUID
-                var fileGUID = systemMethods.GetFileGUIDFromJObject(jsonObject);
-                fileId = informationMethods.File_GetFileIdByFileGUID(fileGUID);
-
-                //Check if FileType has been passed through
-                var fileType = systemMethods.GetFileTypeFromJObject(jsonObject);
-                var fileTypeId = 0L;
-
-                if(!string.IsNullOrWhiteSpace(fileType))
-                {
-                    //FileType was passed through so get the FileTypeId
-                    fileTypeId = informationMethods.FileType_GetFileTypeIdByFileTypeDescription(fileType);
-
-                    if(fileTypeId == 0)
-                    {
-                        //An invalid FileType was passed through so error
-                        systemMethods.ProcessQueue_UpdateEffectiveToDateTime(processQueueGUID, determineFileTypeAPIId, true, $"Invalid FileType {fileType} provided for FileId {fileId}");
-                        return;
-                    }
-                }
-                else
-                {
-                    //TODO: Read file to determine what type it is
-                    if(fileTypeId == 0)
-                    {
-                        //A FileType could not be determined so error
-                        systemMethods.ProcessQueue_UpdateEffectiveToDateTime(processQueueGUID, determineFileTypeAPIId, true, $"Unable to determine FileType for FileId {fileId}");
-                        return;
-                    }
-                }
-
-                var mappingMethods = new Methods.MappingSchema();
-
-                //Insert File To FileType Mapping
-                mappingMethods.FileToFileType_Insert(createdByUserId, sourceId, fileId, fileTypeId);
-
-                //Get ProcessGUID related to FileType
-                var processId = mappingMethods.FileTypeToProcess_GetProcessIdByFileTypeId(fileTypeId);
-                var processGUID = systemMethods.Process_GetProcessGUIDByProcessId(processId);
-
-                //Add ProcessGUID into jsonObject
-                jsonObject.Add(new Enums.SystemSchema.API.RequiredDataKey().ProcessGUID, processGUID);
-
-                //Create new ProcessQueueGUID
-                var newProcessQueueGUID = Guid.NewGuid().ToString();
-
-                //Link ProcessQueueGUIDs
-                systemMethods.ProcessQueueProgression_Insert(createdByUserId, sourceId, processQueueGUID, newProcessQueueGUID);
-
-                //Update ProcessQueueGUID in jsonObject
-                systemMethods.SetProcessQueueGUIDInJObject(jsonObject, newProcessQueueGUID);
-
-                //Get Routing.API URL
-                var routingAPIId = systemAPIMethods.GetRoutingAPIId();
-
-                //Connect to Routing API and POST data
-                systemAPIMethods.PostAsJsonAsync(routingAPIId, systemAPIGUIDEnums.DetermineFileTypeAPI, hostEnvironment, jsonObject);
-
-                //Update Process Queue
-                systemMethods.ProcessQueue_UpdateEffectiveToDateTime(processQueueGUID, determineFileTypeAPIId, false, null);
-            }
-            catch(Exception error)
-            {
-                var errorId = systemMethods.InsertSystemError(createdByUserId, sourceId, error);
-
-                //Update Process Queue
-                systemMethods.ProcessQueue_UpdateEffectiveToDateTime(processQueueGUID, determineFileTypeAPIId, true, $"System Error Id {errorId}");
-            }
+            var fileName = @"C:\wamp64\www\Website\Code\Login\DetermineFileTypeApp\bin\Debug\netcoreapp3.1\DetermineFileTypeApp.exe";
+            new Methods.SystemSchema.Application().LaunchApplication(
+                data, 
+                new Enums.SystemSchema.API.GUID().DetermineFileTypeAPI, 
+                determineFileTypeAPIId, 
+                hostEnvironment, 
+                fileName
+            );
         }
     }
 }
--- Code/FileUpload/UploadFile.api/Controllers/UploadFileController.cs
+++ Code/FileUpload/UploadFile.api/Controllers/UploadFileController.cs
@@ -6,8 +6,6 @@ using enums;
 using Newtonsoft.Json.Linq;
 using System;
 using Microsoft.Extensions.Configuration;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace UploadFile.api.Controllers
 {
@@ -45,78 +43,14 @@ namespace UploadFile.api.Controllers
         [Route("UploadFile/Upload")]
         public void Upload([FromBody] object data)
         {
-            var informationMethods = new Methods.InformationSchema();
-            var systemMethods = new Methods.SystemSchema();
-
-            //Get base variables
-            var createdByUserId = new Methods.AdministrationSchema.User().GetSystemUserId();
-            var sourceId = informationMethods.GetSystemUserGeneratedSourceId();
-
-            //Get Queue GUID
-            var jsonObject = JObject.Parse(data.ToString());
-            var processQueueGUID = systemMethods.GetProcessQueueGUIDFromJObject(jsonObject);
-
-            try
-            {
-                //Insert into ProcessQueue
-                systemMethods.ProcessQueue_Insert(
-                    processQueueGUID, 
-                    createdByUserId,
-                    sourceId,
-                    uploadFileAPIId);
-
-                if(!new Methods.SystemSchema.API().PrerequisiteAPIsAreSuccessful(new Enums.SystemSchema.API.GUID().UploadFileAPI, uploadFileAPIId, hostEnvironment, jsonObject))
-                {
-                    return;
-                }
-
-                //Update Process Queue
-                systemMethods.ProcessQueue_UpdateEffectiveFromDateTime(processQueueGUID, uploadFileAPIId);
-
-                var informationFileAttributeEnums = new Enums.InformationSchema.File.Attribute();
-
-                //Insert FileGUID into Information.File
-                var fileGUID = systemMethods.GetFileGUIDFromJObject(jsonObject);
-                informationMethods.File_Insert(createdByUserId, sourceId, fileGUID);
-
-                //Get FileId by FileGUID
-                var fileId = informationMethods.File_GetFileIdByFileGUID(fileGUID);
-
-                Parallel.ForEach(new List<int>{1, 2, 3}, process => {
-                    if(process == 1)
-                    {
-                        //Get FileName File Attribute Id
-                        var fileNameFileAttributeId = informationMethods.FileAttribute_GetFileAttributeIdByFileAttributeDescription(informationFileAttributeEnums.FileName);
-
-                        //Insert File Name into Information.FileDetail
-                        var fileName = systemMethods.GetFileNameFromJObject(jsonObject);
-                        informationMethods.FileDetail_Insert(createdByUserId, sourceId, fileId, fileNameFileAttributeId, fileName);
-                    }
-                    else if(process == 2)
-                    {
-                        //Get ProcessQueueGUID File Attribute Id
-                        var processQueueGUIDFileAttributeId = informationMethods.FileAttribute_GetFileAttributeIdByFileAttributeDescription(informationFileAttributeEnums.ProcessQueueGUID);
-
-                        //Insert Process Queue GUID into Information.FileDetail
-                        informationMethods.FileDetail_Insert(createdByUserId, sourceId, fileId, processQueueGUIDFileAttributeId, processQueueGUID);
-                    }
-                    else {
-                        //Insert File Content into Information.FileContent
-                        var fileContent = systemMethods.GetFileContentFromJObject(jsonObject);
-                        informationMethods.FileContent_Insert(createdByUserId,sourceId, fileId, fileContent);
-                    }
-                });
-                
-                //Update Process Queue
-                systemMethods.ProcessQueue_UpdateEffectiveToDateTime(processQueueGUID, uploadFileAPIId, false, null);
-            }
-            catch(Exception error)
-            {
-                var errorId = systemMethods.InsertSystemError(createdByUserId, sourceId, error);
-
-                //Update Process Queue
-                systemMethods.ProcessQueue_UpdateEffectiveToDateTime(processQueueGUID, uploadFileAPIId, true, $"System Error Id {errorId}");
-            }
+            var fileName = @"C:\wamp64\www\Website\Code\Login\UploadFileApp\bin\Debug\netcoreapp3.1\UploadFileApp.exe";
+            new Methods.SystemSchema.Application().LaunchApplication(
+                data, 
+                new Enums.SystemSchema.API.GUID().UploadFileAPI, 
+                uploadFileAPIId, 
+                hostEnvironment, 
+                fileName
+            );
         }
     }
 }
