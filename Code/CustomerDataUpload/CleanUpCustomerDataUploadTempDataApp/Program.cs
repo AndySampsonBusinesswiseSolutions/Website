245ca4da672845b9f795ab184aa8617652990f21
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 17:56:33 +0000

    Update APIs to Apps
    Create CleanUpCustomerDataUploadTempData App

---
+++ Code/CustomerDataUpload/CleanUpCustomerDataUploadTempDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanUpCustomerDataUploadTempDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
