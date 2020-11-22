0392a1cf6ecb3c8d41b9b84fdb66fa414eddfdf2
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:06:06 +0000

    Update APIs to Apps
    Create CommitMeterToMeterTimeswitchCodeDataApp

---
+++ Code/CustomerDataUpload/CommitMeterToMeterTimeswitchCodeDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitMeterToMeterTimeswitchCodeDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
