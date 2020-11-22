1f0d1db8fcf602756a2004718fad0ccf14bec157
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:11:28 +0000

    Update APIs to Apps
    Create StoreFlexTradeDataApp

---
+++ Code/CustomerDataUpload/StoreFlexTradeDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreFlexTradeDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
