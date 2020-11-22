e49bd89b934d3bd031efc27749d39421916328fc
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 17:57:50 +0000

    Update APIs to Apps
    Create CommitAreaToMeterDataApp

---
+++ Code/CustomerDataUpload/CommitAreaToMeterDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitAreaToMeterDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
