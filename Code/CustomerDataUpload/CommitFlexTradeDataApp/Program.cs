4d3c928b6bad18c6ece8059bb496e4a693c31fce
Andy Sampson <andy.sampson@businesswisesolutions.local>
2020-11-22 18:03:47 +0000

    Update APIs to Apps
    Create CommitFlexTradeDataApp

---
+++ Code/CustomerDataUpload/CommitFlexTradeDataApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitFlexTradeDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
